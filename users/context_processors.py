f47257edf0f3db5d3673ab155426ea124e936afa
karim <karimabdelhakim22@hotmail.com>
2018-11-29 16:54:33 +0200

    check if user is authenticated in brand context

--- users/context_processors.py
+++ users/context_processors.py
@@ -1,6 +1,8 @@
 from users.models import Brand
 
 def brand_context(request):
+    if not request.user.is_authenticated:
+        return {}
     brand_qs = Brand.objects.filter(hierarchy=request.user.hierarchy)
     if brand_qs.exists():
         brand = brand_qs.first()

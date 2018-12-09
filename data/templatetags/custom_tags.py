fcf307420a9f669a887486be93cf3f216fb4c48f
karim <karimabdelhakim22@hotmail.com>
2018-12-09 14:47:53 +0200

    add filter to invert direction , helpfull in pull-right and pull-left

--- data/templatetags/custom_tags.py
+++ data/templatetags/custom_tags.py
@@ -18,3 +18,12 @@ def add_class(field, class_name):
     return field.as_widget(attrs={
         "class": " ".join((field.css_classes(), class_name))
     })
+
+
+@register.filter(name='invert_dir')
+def invert_dir(dir, language_code):
+        if language_code == 'en':
+            return dir
+        if dir == 'left':
+            return 'right'
+        return 'left'

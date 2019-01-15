708ee409f9abf2dcb6fbf58d02c5e017de4ed737
karim <karimabdelhakim22@hotmail.com>
2019-01-15 18:16:21 +0200

    update setup model with format and collection_data steps

--- users/models/setup.py
+++ users/models/setup.py
@@ -6,16 +6,21 @@ class Setup(models.Model):
     """
     Setup model to save the state of the entity root setup
     """
+    # root user
     user = models.OneToOneField('users.User', on_delete=models.CASCADE)
     levels_setup = models.BooleanField(default=False)
     users_setup = models.BooleanField(default=False)
     category_setup = models.BooleanField(default=False)
+    format_setup = models.BooleanField(default=False)
+    collection_data_setup = models.BooleanField(default=False)
 
     def __str__(self):
         return '{0}_setup'.format(str(self.user))
 
     def can_pass(self):
-        if all([self.levels_setup, self.users_setup, self.category_setup]):
+        if all([self.levels_setup, self.users_setup, self.category_setup, self.format_setup]):
+            if self.user.has_perm('users.has_collection'):
+                return self.collection_data_setup
             return True
         return False
 
@@ -27,10 +32,25 @@ class Setup(models.Model):
     @cached_property
     def percentage(self):
         per = 0
-        if self.levels_setup:
-            per += 30
-            if self.users_setup:
-                per += 40
-                if self.category_setup:
-                    per += 30
+        if self.user.has_perm('users.has_collection'):
+            if self.levels_setup:
+                per += 20
+                if self.users_setup:
+                    per += 20
+                    if self.category_setup:
+                        per += 20
+                        if self.format_setup:
+                            per += 20
+                            if self.collection_data_setup:
+                                per += 20
+        # users.has_disbursement
+        else:
+            if self.levels_setup:
+                per += 25
+                if self.users_setup:
+                    per += 25
+                    if self.category_setup:
+                        per += 25
+                        if self.format_setup:
+                            per += 25
         return per

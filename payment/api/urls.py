19a0bb1bcb77d3d09f5917e116ea0366aad21216
karim <karimabdelhakim22@hotmail.com>
2019-06-16 17:45:42 +0200

    make ExpiringAuthToken work with django-axes

--- payment/api/urls.py
+++ payment/api/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-from rest_framework_expiring_authtoken import views
-
 from .views import TransactionCallbackAPIView
 
 app_name = 'payment_api'
 urlpatterns = [
-    path('token-auth/', views.obtain_expiring_auth_token),
     path('trx/', TransactionCallbackAPIView.as_view())
 ]

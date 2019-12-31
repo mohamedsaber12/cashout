1ad991ec1afe82ed459b4edfebba93598eff61d2
Mohammad Mamdouh <mohamedmamdouh@paymobsolutions.com>
2019-12-31 16:14:36 +0200

    Improve: Update production settings for the new domain name

--- disbursement/settings/production.py
+++ disbursement/settings/production.py
@@ -3,7 +3,8 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS += (
-    'payroll.paymobsolutions.com',
+    'payouts.paymobsolutions.com',
+    '54.91.163.127',
 )
 
 INSTALLED_APPS += (
@@ -17,13 +18,12 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': env.str('DB_NAME'),
         'USER': env.str('DB_USER'),
-        'PASSWORD': env.str('DB_PASS'),
+        'PASSWORD': env.str('DB_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '',
         'CONN_MAX_AGE': 600,
     }
 }
-
 
 # Email
 SERVER_EMAIL = 'confirmrequest@paymobsolutions.com'
@@ -34,25 +34,25 @@ EMAIL_HOST_USER = env.str('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
 
 # Email Reporting
-
-ADMINS = [('Amir Raouf', 'amirraouf@paymobsolutions.com'),
-          ('karim abdelhakim', 'karimabdelhakim@paymobsolutions.com'),
-          ]
-# celery
-
-CELERY_BROKER_URL = 'amqp://paymobsecure:(!~)qwe!~@localhost//'
+ADMINS = [
+    ('Mohamed Mamdouh', 'mohamedmamdouh@paymobsolutions.com'),
+    ('Omar Elraies', 'omarelraies@paymobsolutions.com'),
+    ('Hesham Sayed', 'heshamsayed@paymobsolutions.com'),
+]
 
 # ssl
-
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE   = True
+CSRF_COOKIE_SECURE      = True
+# SECURE_SSL_REDIRECT   = True        # Handled by nginx
 
 # base url
-BASE_URL = 'https://payroll.paymobsolutions.com'
+BASE_URL = 'https://payouts.paymobsolutions.com'
 
 # session expiration
 SESSION_EXPIRE_SECONDS = 300
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
+
+# Celery
+CELERY_BROKER_URL = env.str("CELERY_BROKER_URL")

d84c6050e5673cb2f5070235b049e3859fe6a474
Mohammad Mamdouh <mohamedmamdouh@paymobsolutions.com>
2020-02-10 22:00:09 +0200

    Improve: Extract msisdn and wallet number validations to a separate validators

--- instant_cashin/api/serializers.py
+++ instant_cashin/api/serializers.py
@@ -1,40 +1,12 @@
-from django.utils.translation import gettext_lazy as _
-
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
 
-from core.utils.validations import phonenumber_form_validate
+from .validators import msisdn_validator, issuer_validator
 
 
 class InstantUserInquirySerializer(serializers.Serializer):
     """
     Serializes instant user/wallet inquiry requests
     """
-    msisdn = serializers.CharField(max_length=11, required=True)
-    issuer = serializers.CharField(max_length=12, required=True)
-    unique_identifier = serializers.CharField(max_length=255, required=True)
-
-    def validate(self, attrs):
-        """
-        Validate the request attributes
-        :return:
-        """
-        msisdn = f"+2{attrs.get('msisdn')}"
-        issuer = attrs.get("issuer")
-        unique_identifier = attrs.get("unique_identifier")
-
-        # Validate that the issuer is VodafoneCash
-        if issuer != "VodafoneCash":
-            msg = _("You are not permitted to inquire at an issuer other that 'VodafoneCash'")
-            raise serializers.ValidationError(msg)
-
-        # Validate wallet number is a valid EG phone number
-        try:
-            phonenumber_form_validate(msisdn)
-        except ValidationError as e:
-            msg = _("Invalid phone number")
-            raise serializers.ValidationError(msg)
-
-        # Validate unique issuer
-
-        return attrs
+    msisdn = serializers.CharField(max_length=11, required=True, validators=[msisdn_validator])
+    issuer = serializers.CharField(max_length=12, required=True, validators=[issuer_validator])
+    unique_identifier = serializers.CharField(max_length=255, required=True)    # Add validations/rate limit
---
+++ instant_cashin/api/validators.py
@@ -0,0 +1,36 @@
+from django.utils.translation import gettext_lazy as _
+
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from core.utils.validations import phonenumber_form_validate
+
+
+def msisdn_validator(msisdn):
+    """
+    Validator for the wallet to consume the cash in
+    :param msisdn: the wallet/phone-number being inquired at
+    :return:
+    """
+    msisdn = f"+2{msisdn}"
+
+    try:
+        phonenumber_form_validate(msisdn)
+    except ValidationError:
+        msg = _("Invalid phone number")
+        raise serializers.ValidationError(msg)
+
+    return msisdn
+
+
+def issuer_validator(issuer):
+    """
+    Validate that the issuer being used is VodafoneCash
+    :param issuer: the type of wallet to check
+    :return:
+    """
+    if issuer != "VodafoneCash":
+        msg = _("You are not permitted to inquire at an issuer other that 'VodafoneCash'")
+        raise serializers.ValidationError(msg)
+
+    return issuer

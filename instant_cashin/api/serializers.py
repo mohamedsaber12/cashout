5efe5d18f5aa83c3408c7d2f15653a3c40c7029e
Mohammad Mamdouh <mohamedmamdouh@paymobsolutions.com>
2020-02-11 14:37:59 +0200

    Feature: Added instant disbursement serializer

--- instant_cashin/api/serializers.py
+++ instant_cashin/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .validators import msisdn_validator, issuer_validator
+from .validators import fees_validator, issuer_validator, msisdn_validator, user_budget_validator
 
 
 class InstantUserInquirySerializer(serializers.Serializer):
@@ -10,3 +10,15 @@ class InstantUserInquirySerializer(serializers.Serializer):
     msisdn = serializers.CharField(max_length=11, required=True, validators=[msisdn_validator])
     issuer = serializers.CharField(max_length=12, required=True, validators=[issuer_validator])
     unique_identifier = serializers.CharField(max_length=255, required=True)    # Add validations/rate limit
+
+
+class InstantDisbursementSerializer(serializers.Serializer):
+    """
+    Serializes instant disbursement requests
+    """
+    msisdn = serializers.CharField(max_length=11, required=True, validators=[msisdn_validator])
+    amount = serializers.IntegerField(required=True, validators=[user_budget_validator])
+    pin = serializers.CharField(min_length=6, max_length=6, required=False, allow_null=True, allow_blank=True)
+    fees = serializers.CharField(
+            max_length=4, required=False, allow_blank=True, allow_null=True, validators=[fees_validator]
+    )

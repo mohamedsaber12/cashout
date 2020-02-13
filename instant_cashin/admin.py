2ef4e1dfb1ebd41fe7c2ab9ac49ee9ffc423228c
Mohammad Mamdouh <mohamedmamdouh@paymobsolutions.com>
2020-02-13 18:20:50 +0200

    Feature: Added new admin class for budget model

--- instant_cashin/admin.py
+++ instant_cashin/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-from .models.instant_transactions import InstantTransaction
+from .models import Budget, InstantTransaction
 
 
 class InstantTransactionAdmin(admin.ModelAdmin):
+    """
+    Instant Transaction Admin model for the Instant Transaction model
+    """
 
     default_fields = ['uid', 'from_user', 'anon_sender', 'anon_recipient', 'status']
     fields =  default_fields + ['amount', 'failure_reason']
@@ -18,4 +22,38 @@ class InstantTransactionAdmin(admin.ModelAdmin):
         return self.readonly_fields
 
 
+class BudgetAdmin(admin.ModelAdmin):
+    """
+    Budget Admin model for the Budget model
+    """
+
+    list_display = ['disburser', 'created_by', 'total_disbursed_amount', 'disbursed_amount', 'max_amount', 'updated_at']
+    readonly_fields = ['total_disbursed_amount', 'updated_at', 'created_at', 'created_by']
+    ordering = ['-updated_at']
+
+    fieldsets = (
+        (_("Users Details"), {'fields': ('disburser', 'created_by')}),
+        (_("Budget Amount Details"), {'fields': ('total_disbursed_amount', 'max_amount', 'disbursed_amount')}),
+        (_("Important Dates"), {'fields': ('updated_at', 'created_at')})
+    )
+
+    def get_readonly_fields(self, request, obj=None):
+        readonly_fields = super().get_readonly_fields(request, obj)
+        if not request.user.is_superuser or not request.user.is_superadmin:
+            return readonly_fields + self.list_display
+        return readonly_fields
+
+    def has_add_permission(self, request):
+        if not request.user.is_superuser or not request.user.is_superadmin:
+            raise PermissionError(_("Only super users allowed to add to this table."))
+        return True
+
+    def save_model(self, request, obj, form, change):
+        if not request.user.is_superuser or not request.user.is_superadmin:
+            raise PermissionError(_("Only super users allowed to create/update at this table."))
+        obj.created_by = request.user
+        obj.save()
+
+
 admin.site.register(InstantTransaction, InstantTransactionAdmin)
+admin.site.register(Budget, BudgetAdmin)

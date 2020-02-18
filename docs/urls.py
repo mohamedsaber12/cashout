ea4bc920315f5e6fa54e9c348c347ab5ddf49bf3
Mohammad Mamdouh <mohamedmamdouh@paymobsolutions.com>
2020-02-18 12:28:51 +0200

    Feature: Name docs urlpattern to be used at templates

--- docs/urls.py
+++ docs/urls.py
@@ -3,6 +3,9 @@ from django.urls import re_path
 from .views import serve_docs
 
 
+app_name = 'docs'
+
+
 urlpatterns = [
-    re_path(r'^docs/(?P<path>.*)$', serve_docs),
+    re_path(r'^docs/(?P<path>.*)$', serve_docs, name='instant_docs'),
 ]

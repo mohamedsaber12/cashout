44326d5e0d8fead95a217c16bfcbc4438a6ec3ca
Mohammad Mamdouh <mohamedmamdouh@paymobsolutions.com>
2020-03-03 15:19:02 +0200

    Improve: Reformat levels model to be pep8 compliant

--- users/models/levels.py
+++ users/models/levels.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+
 LEVELS = (
     (1, 'Level 1'),
     (2, 'Level 2'),
@@ -11,34 +12,33 @@ LEVELS = (
 
 class Levels(models.Model):
     """
-    Model reference for levels of authority
-    of users in the hierarchy tree
+    Model reference for levels of authority of users in the hierarchy tree
     Added by the root of the tree only
     """
-    max_amount_can_be_disbursed = models.FloatField(
-        default=0, verbose_name=_('Max amount can be disbursed'))
-    level_of_authority = models.PositiveSmallIntegerField(
-        choices=LEVELS, null=True, blank=True)
-    # root
+    max_amount_can_be_disbursed = models.FloatField(default=0, verbose_name=_('Max amount can be disbursed'))
+    level_of_authority = models.PositiveSmallIntegerField(choices=LEVELS, null=True, blank=True)
     created = models.ForeignKey(
-        'users.User', on_delete=models.CASCADE, null=True)
-
-    def __str__(self):
-        return f'amount is {self.max_amount_can_be_disbursed} of level {self.level_of_authority}'
+            'users.User',
+            on_delete=models.CASCADE,
+            null=True,
+            help_text=_('Root user who created the levels, and have the ability to modify their values.')
+    )
 
     class Meta:
-        ordering = ('max_amount_can_be_disbursed',)
+        ordering = ('max_amount_can_be_disbursed', )
+
+    def __str__(self):
+        return f'Amount is {self.max_amount_can_be_disbursed} of level {self.level_of_authority}'
 
     @classmethod
-    def update_levels_authority(cls,root):
+    def update_levels_authority(cls, root):
         """
         order levels in ascending order by max_amount_can_be_disbursed and 
         give them a level number(level_of_authority).
         """
         levels_qs = cls.objects.filter(created=root)
         levels_values = list(levels_qs.values_list('max_amount_can_be_disbursed', flat=True))
-        for level in levels_qs:
-            level.level_of_authority = levels_values.index(
-                level.max_amount_can_be_disbursed) + 1
-            level.save()    
 
+        for level in levels_qs:
+            level.level_of_authority = levels_values.index(level.max_amount_can_be_disbursed) + 1
+            level.save()    

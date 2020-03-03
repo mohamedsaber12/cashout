d7508f7e13c82bff0caba9b9b2a371a06bea76df
Mohammad Mamdouh <mohamedmamdouh@paymobsolutions.com>
2020-03-03 15:00:49 +0200

    Improve: Reformat data views to be pep8 compliant

--- data/views.py
+++ data/views.py
@@ -7,8 +7,7 @@ import xlrd
 
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.http import (Http404, HttpResponse, HttpResponseRedirect,
-                         JsonResponse)
+from django.http import (Http404, HttpResponse, HttpResponseRedirect, JsonResponse)
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from django.utils import translation
@@ -21,8 +20,7 @@ from users.decorators import (collection_users, disbursement_users, root_only,
                               root_or_maker_or_uploader, setup_required)
 from users.models import CheckerUser, Levels
 
-from .forms import (DocReviewForm, FileCategoryFormSet, FileDocumentForm,
-                    FormatFormSet)
+from .forms import (DocReviewForm, FileCategoryFormSet, FileDocumentForm, FormatFormSet)
 from .models import CollectionData, Doc, DocReview, FileCategory, Format
 from .tasks import (doc_review_maker_mail, handle_disbursement_file,
                     handle_uploaded_file, notify_checkers, notify_disbursers)
@@ -30,9 +28,9 @@ from .utils import get_client_ip
 
 
 UPLOAD_LOGGER = logging.getLogger("upload")
+UPLOAD_ERROR_LOGGER = logging.getLogger("upload_error")
 DELETED_FILES_LOGGER = logging.getLogger("deleted_files")
 UNAUTHORIZED_FILE_DELETE_LOGGER = logging.getLogger("unauthorized_file_delete")
-UPLOAD_ERROR_LOGGER = logging.getLogger("upload_error")
 DOWNLOAD_LOGGER = logging.getLogger("download_serve")
 VIEW_DOCUMENT_LOGGER = logging.getLogger("view_document")
 
@@ -46,6 +44,7 @@ def redirect_home(request):
     if request.user.is_instantapiviewer:
         return redirect(reverse('users:instant_transactions'))
     status = request.user.get_status(request)
+
     return redirect(f'data:{status}_home')
 
 
@@ -57,50 +56,41 @@ def disbursement_home(request):
     POST:
     View that allows the maker with a file category to upload a disbursement file.
     The file is later processed by the task 'handle_disbursement_file'.
+
     GET:
     View that list all documents related to logged in user hierarchy.
     Documents can be filtered by date.
     Documents are paginated ("docs_paginated") but not used in template.
     """
-    format_qs = FileCategory.objects.get_by_hierarchy(
-        request.user.hierarchy)
+    format_qs = FileCategory.objects.get_by_hierarchy(request.user.hierarchy)
     can_upload = bool(format_qs)
     user_has_upload_perm = request.user.is_maker or request.user.is_upmaker
+
     if request.method == 'POST' and can_upload and user_has_upload_perm and request.user.root.client.is_active:
-        form_doc = FileDocumentForm(
-            request.POST, request.FILES, request=request, is_disbursement=True)
+        form_doc = FileDocumentForm(request.POST, request.FILES, request=request, is_disbursement=True)
 
         if form_doc.is_valid():
             file_doc = form_doc.save()
             UPLOAD_LOGGER.debug(f"""[DISBURSEMENT FILE UPLOAD]
             User: {request.user.username} from IP Address {get_client_ip(request)}
             Uploaded disbursement file with doc_id: {file_doc.id}""")
-            handle_disbursement_file.delay(
-                file_doc.id, language=translation.get_language())
-
-            # Redirect to the document list after POST
-            return HttpResponseRedirect(request.path)
+            handle_disbursement_file.delay(file_doc.id, language=translation.get_language())
+            return HttpResponseRedirect(request.path)           # Redirect to the document list after POST
         else:
             UPLOAD_ERROR_LOGGER.debug(f"""[DISBURSEMENT FILE UPLOAD ERROR]
             Disbursement upload error: {form_doc.errors['file'][0]}
             By User: {request.user.username} from IP Address {get_client_ip(request)}""")
             return JsonResponse(form_doc.errors, status=400)
 
-    doc_list_disbursement = Doc.objects.filter(
-        owner__hierarchy=request.user.hierarchy,
-        type_of=Doc.DISBURSEMENT)
-
+    doc_list_disbursement = Doc.objects.filter(owner__hierarchy=request.user.hierarchy, type_of=Doc.DISBURSEMENT)
     paginator = Paginator(doc_list_disbursement, 10)
     page = request.GET.get('page')
     docs = paginator.get_page(page)
-
-
-
     context = {
         'doc_list_disbursement': docs,
         'format_qs': format_qs,
         'can_upload': can_upload,
-        'user_has_upload_perm': user_has_upload_perm,
+        'user_has_upload_perm': user_has_upload_perm
     }
 
     return render(request, 'data/disbursement_home.html', context=context)
@@ -114,40 +104,34 @@ def collection_home(request):
     POST:
     View that allows the maker with a file category to upload a disbursement file.
     The file is later processed by the task 'handle_disbursement_file'.
+
     GET:
     View that list all documents related to logged in user hierarchy.
     Documents can be filtered by date.
     Documents are paginated ("docs_paginated") but not used in template.
     """
     format_qs = Format.objects.filter(hierarchy=request.user.hierarchy)
-    collection = CollectionData.objects.filter(
-        user__hierarchy=request.user.hierarchy).first()
+    collection = CollectionData.objects.filter(user__hierarchy=request.user.hierarchy).first()
     can_upload = bool(collection)
     user_has_upload_perm = request.user.is_uploader or request.user.is_upmaker
-    if (request.method == 'POST' and can_upload and user_has_upload_perm and request.user.root.client.is_active):
-        form_doc = FileDocumentForm(
-            request.POST, request.FILES, request=request, collection=collection)
+
+    if request.method == 'POST' and can_upload and user_has_upload_perm and request.user.root.client.is_active:
+        form_doc = FileDocumentForm(request.POST, request.FILES, request=request, collection=collection)
 
         if form_doc.is_valid():
             file_doc = form_doc.save()
             UPLOAD_LOGGER.debug(f"""[COLLECTION FILE UPLOAD]
             User: {request.user.username} from IP Address {get_client_ip(request)}
             Uploaded collection file with doc_id: {file_doc.id}""")
-            handle_uploaded_file.delay(
-                file_doc.id, language=translation.get_language())
-
-            # Redirect to the document list after POST
-            return HttpResponseRedirect(request.path)
+            handle_uploaded_file.delay(file_doc.id, language=translation.get_language())
+            return HttpResponseRedirect(request.path)               # Redirect to the document list after POST
         else:
             UPLOAD_ERROR_LOGGER.debug(f"""[COLLECTION FILE UPLOAD ERROR]
             Collection upload error: {form_doc.errors['file'][0]}
             By User: {request.user.username} from IP Address {get_client_ip(request)}""")
             return JsonResponse(form_doc.errors, status=400)
 
-    doc_list_collection = Doc.objects.filter(
-        owner__hierarchy=request.user.hierarchy,
-        type_of=Doc.COLLECTION)
-
+    doc_list_collection = Doc.objects.filter(owner__hierarchy=request.user.hierarchy, type_of=Doc.COLLECTION)
     context = {
         'doc_list_collection': doc_list_collection,
         'format_qs': format_qs,
@@ -164,17 +148,17 @@ class FileDeleteView(View):
     Function that deletes a file
     """
 
-    def post(self, request,pk=None, *args, **kwargs):
+    def post(self, request, pk=None, *args, **kwargs):
         if not request.is_ajax():
             return JsonResponse(data={}, status=403)
-    
-        file_obj = get_object_or_404(
-            Doc, pk=pk, owner__hierarchy=request.user.hierarchy)
+
+        file_obj = get_object_or_404(Doc, pk=pk, owner__hierarchy=request.user.hierarchy)
         file_obj.delete()
         DELETED_FILES_LOGGER.debug(f"""[FILE DELETED]
         User: {request.user.username} from IP Address {get_client_ip(request)}
         Deleted a file with doc_name: {file_obj.filename()}""")
-        return JsonResponse(data={},status=200)
+
+        return JsonResponse(data={}, status=200)
 
 
 @login_required
@@ -190,24 +174,15 @@ def document_view(request, doc_id):
     doc = get_object_or_404(Doc, id=doc_id)
     review_form_errors = None
     reviews = None
-    # True if checker already reviewed this doc or reviews are completed
-    hide_review_form = True
-    # True if checker have the level rights to review the doc
-    can_review = True
+    hide_review_form = True         # True if checker already reviewed this doc or reviews are completed
+    can_review = True               # True if checker have the level rights to review the doc
     can_user_disburse = {}
     if doc.owner.hierarchy == request.user.hierarchy:
-        # doc already dibursed
-        if (
-                doc.is_disbursed and 
-                (
-                    doc.owner == request.user or
-                    request.user.is_checker or
-                    request.user.is_root
-                )
-            ):
-            return redirect(reverse("disbursement:disbursed_data", args=(doc_id,)))
+        # doc already disbursed
+        if doc.is_disbursed and (doc.owner == request.user or request.user.is_checker or request.user.is_root):
+            return redirect(reverse("disbursement:disbursed_data", args=(doc_id, )))
         if request.user.is_checker:
-            # makers must notify checkers and allow the document to be dibursed
+            # makers must notify checkers and allow the document to be disbursed
             # checker should not have access to doc url or id before that
             if not doc.can_be_disbursed:
                 VIEW_DOCUMENT_LOGGER.debug(f"""[DOC CAN NOT BE DISBURSED]
@@ -225,11 +200,9 @@ def document_view(request, doc_id):
                         can_review = False
                     # user can review then user review doesn't exist then show review form
                     hide_review_form = user_review_exist
-
             reviews = DocReview.objects.filter(doc=doc)
 
             if request.method == "POST" and not hide_review_form:
-
                 doc_review_form = DocReviewForm(request.POST)
                 
                 if doc_review_form.is_valid():
@@ -237,22 +210,21 @@ def document_view(request, doc_id):
                     doc_review_obj.user_created = request.user
                     doc_review_obj.doc = doc
                     doc_review_obj.save()
-
                     ready_for_disbursement = doc.is_reviews_completed()
+
                     if ready_for_disbursement:
-                        matched_levels_can_disburse = Levels.objects.filter(created=request.user.root). \
-                            filter(max_amount_can_be_disbursed__gte=doc.total_amount)
+                        matched_levels_can_disburse = Levels.objects.filter(created=request.user.root).filter(
+                                max_amount_can_be_disbursed__gte=doc.total_amount)
                         levels_of_authority = [level.level_of_authority for level in matched_levels_can_disburse]
-                        notify_disbursers.delay(
-                            doc.id, min(levels_of_authority), language=translation.get_language())
+                        notify_disbursers.delay(doc.id, min(levels_of_authority), language=translation.get_language())
 
                     # notify checkers of next level if ?
                     # 1 current checker review is ok, and
                     # 2 doc is not ready for disbursement, and
                     # 3 all of the current level of checkers has already reviewed the doc
                     current_level_checkers_can_review = False
-                    current_level_checkers = CheckerUser.objects.filter(hierarchy=request.user.hierarchy).\
-                            filter(level__level_of_authority=request.user.level.level_of_authority)
+                    current_level_checkers = CheckerUser.objects.filter(hierarchy=request.user.hierarchy).filter(
+                            level__level_of_authority=request.user.level.level_of_authority)
                     for checker in current_level_checkers:
                         can_user_review, user_already_reviewed = doc.can_user_review(checker)
                         if can_user_review:
@@ -260,16 +232,13 @@ def document_view(request, doc_id):
 
                     if doc_review_obj.is_ok and not ready_for_disbursement and not current_level_checkers_can_review:
                         levels = CheckerUser.objects.filter(hierarchy=request.user.hierarchy).values_list(
-                            'level__level_of_authority', flat=True)
+                                'level__level_of_authority', flat=True)
                         levels = list(levels)
-                        levels = [level for level in levels if level >
-                                  request.user.level.level_of_authority]
+                        levels = [level for level in levels if level > request.user.level.level_of_authority]
                         if levels:
-                            notify_checkers.delay(
-                                doc.id, min(levels), language=translation.get_language())
+                            notify_checkers.delay(doc.id, min(levels), language=translation.get_language())
                     # notify the maker either way
-                    doc_review_maker_mail.delay(
-                        doc.id, doc_review_obj.id, language=translation.get_language())
+                    doc_review_maker_mail.delay(doc.id, doc_review_obj.id, language=translation.get_language())
                     hide_review_form = True
                 else:
                     review_form_errors = doc_review_form.errors['comment'][0]
@@ -320,6 +289,7 @@ def protected_serve(request, path, document_root=None, show_indexes=False):
     User: {request.user.username} from IP Address {get_client_ip(request)}
     Downloaded {path}""")
     path = 'documents/' + path
+
     try:
         doc = get_object_or_404(Doc, file=path)
         if doc.owner.hierarchy == request.user.hierarchy:
@@ -342,10 +312,10 @@ def doc_download(request, doc_id):
     :return: Downloadable excel or 404.
     """
     doc = get_object_or_404(Doc, id=doc_id)
+
     if doc.owner.hierarchy == request.user.hierarchy:
         try:
-            response = HttpResponse(
-                doc.file, content_type='application/vnd.ms-excel')
+            response = HttpResponse(doc.file, content_type='application/vnd.ms-excel')
             response['Content-Disposition'] = 'attachment; filename=%s' % doc.filename()
             response['X-Accel-Redirect'] = '/media/' + doc.file.name
             DOWNLOAD_LOGGER.debug(f"""[DOC DOWNLOADED]
@@ -422,7 +392,7 @@ class RetrieveCollectionData(DetailView):
     http_method_names = ['get']
     template_name = "data/document_viewer_collection.html"
 
-    def get_queryset(self,*args,**kwargs):
+    def get_queryset(self, *args, **kwargs):
         return Doc.objects.filter(id=self.kwargs['pk'])
 
     def get_context_data(self, **kwargs):

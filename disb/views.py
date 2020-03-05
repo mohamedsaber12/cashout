371dfff7c0d8ed9a0d209c8b20a12175f8a5028a
Mohammad Mamdouh <mohamedmamdouh@paymobsolutions.com>
2020-03-06 00:48:03 +0200

    Improve: Reformat disb views module to be pep8 compliant

--- disb/views.py
+++ disb/views.py
@@ -20,14 +20,11 @@ from rest_framework_expiring_authtoken.models import ExpiringToken
 
 from data.decorators import otp_required
 from data.models import Doc
-from data.tasks import (generate_all_disbursed_data,
-                        generate_failed_disbursed_data,
-                        generate_success_disbursed_data)
+from data.tasks import (generate_all_disbursed_data, generate_failed_disbursed_data, generate_success_disbursed_data)
 from data.utils import get_client_ip, redirect_params
 from payouts.utils import get_dot_env
 from users.decorators import setup_required
-from users.mixins import (RootRequiredMixin, SuperFinishedSetupMixin,
-                          SuperRequiredMixin)
+from users.mixins import (RootRequiredMixin, SuperFinishedSetupMixin, SuperRequiredMixin)
 from users.models import EntitySetup
 
 from .forms import AgentForm, AgentFormSet, BalanceInquiryPinForm
@@ -68,8 +65,7 @@ def disburse(request, doc_id):
                                                                                          'utm_redirect': 'success'})
 
         owner = 'true' if doc_obj.owner.hierarchy == request.user.hierarchy else 'false'
-        perm = 'true' if request.user.has_perm(
-            'data.can_disburse') else 'false'
+        perm = 'true' if request.user.has_perm('data.can_disburse') else 'false'
         doc_validated = 'false' if doc_obj.is_disbursed else 'true'
         return redirect_params('data:doc_viewer', kw={'doc_id': doc_id}, params={'disburse': -1,
                                                                                  'utm_redirect': 'success',
@@ -80,6 +76,7 @@ def disburse(request, doc_id):
     else:
         response = reverse_lazy('data:doc_viewer', kwargs={'doc_id': doc_id})
         return redirect(response)
+
 
 @setup_required
 @login_required
@@ -95,18 +92,15 @@ def disbursement_list(request, doc_id):
         doc_obj.is_disbursed
     )
     if can_view:
-        if request.is_ajax(): 
+        if request.is_ajax():
             if request.GET.get('export_failed') == 'true':
-                generate_failed_disbursed_data.delay(doc_id, request.user.id,
-                                    language=translation.get_language())
+                generate_failed_disbursed_data.delay(doc_id, request.user.id, language=translation.get_language())
                 return HttpResponse(status=200)
             elif request.GET.get('export_success') == 'true':
-                generate_success_disbursed_data.delay(doc_id, request.user.id,
-                                    language=translation.get_language())
+                generate_success_disbursed_data.delay(doc_id, request.user.id, language=translation.get_language())
                 return HttpResponse(status=200)
             elif request.GET.get('export_all') == 'true':
-                generate_all_disbursed_data.delay(doc_id, request.user.id,
-                                                      language=translation.get_language())
+                generate_all_disbursed_data.delay(doc_id, request.user.id, language=translation.get_language())
                 return HttpResponse(status=200)
         context = {
             'Ddata': doc_obj.disbursement_data.all(),
@@ -123,7 +117,6 @@ def disbursement_list(request, doc_id):
 @login_required
 def failed_disbursed_for_download(request, doc_id):
     doc_obj = get_object_or_404(Doc, id=doc_id)
-
     can_view = (
         doc_obj.owner.hierarchy == request.user.hierarchy and
         (
@@ -136,10 +129,10 @@ def failed_disbursed_for_download(request, doc_id):
     if not can_view:
         return HttpResponse(status=401)
 
-    filename = request.GET.get('filename',None)
+    filename = request.GET.get('filename', None)
     if not filename:
         raise Http404
-      
+
     file_path = "%s%s%s" % (settings.MEDIA_ROOT, "/documents/disbursement/", filename)
     if os.path.exists(file_path):
         with open(file_path, 'rb') as fh:
@@ -155,13 +148,12 @@ def failed_disbursed_for_download(request, doc_id):
     else:
         raise Http404
 
+
 @setup_required
 @login_required
 def download_failed_validation_file(request, doc_id):
     doc_obj = get_object_or_404(Doc, id=doc_id)
-    can_view = (
-        doc_obj.owner == request.user and request.user.is_maker
-    )
+    can_view = (doc_obj.owner == request.user and request.user.is_maker)
     if not can_view:
         return HttpResponse(status=401)
 
@@ -169,8 +161,7 @@ def download_failed_validation_file(request, doc_id):
     if not filename:
         raise Http404
 
-    file_path = "%s%s%s" % (settings.MEDIA_ROOT,
-                            "/documents/disbursement/", filename)
+    file_path = "%s%s%s" % (settings.MEDIA_ROOT, "/documents/disbursement/", filename)
     if os.path.exists(file_path):
         with open(file_path, 'rb') as fh:
             response = HttpResponse(
@@ -178,24 +169,25 @@ def download_failed_validation_file(request, doc_id):
                 content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
             )
             response['Content-Disposition'] = 'attachment; filename=%s' % filename
-            FAILED_VALIDATION_DOWNLOAD.debug(f"""[DOWNLOAD FAILED VALIDATIONS DISBURSEMENT] 
+            FAILED_VALIDATION_DOWNLOAD.debug(f"""[DOWNLOAD FAILED VALIDATIONS DISBURSEMENT]
             User: {request.user.username}
             Downloaded failed disbursement's file validations with doc_id: {doc_obj.id}""")
             return response
     else:
         raise Http404
 
+
 class SuperAdminAgentsSetup(SuperRequiredMixin, SuperFinishedSetupMixin, View):
     """
-    View for super user to create Agents for the entity. 
+    View for super user to create Agents for the entity.
     """
     template_name = 'entity/add_agent.html'
 
-    def __init__(self,*args,**kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.env = get_dot_env()
 
-    def get_success_url(self,root):
+    def get_success_url(self, root):
         token, created = ExpiringToken.objects.get_or_create(user=root)
         if created:
             return reverse('users:add_fees', kwargs={'token': token.key})
@@ -203,45 +195,45 @@ class SuperAdminAgentsSetup(SuperRequiredMixin, SuperFinishedSetupMixin, View):
             token.delete()
             token = ExpiringToken.objects.create(user=root)
         return reverse('users:add_fees', kwargs={'token': token.key})
-        
 
-    def get(self,request,*args, **kwargs):
+    def get(self, request, *args, **kwargs):
+        """
+        Handles GET requests to the SuperAdminAgentsSetup view
+        """
         root = ExpiringToken.objects.get(key=self.kwargs['token']).user
         context = {
-            'agentform' : AgentFormSet(
-                queryset=Agent.objects.filter(wallet_provider=root,super = False),
+            'agentform': AgentFormSet(
+                queryset=Agent.objects.filter(wallet_provider=root, super=False),
                 prefix='agents',
                 form_kwargs={'root': root}
             ),
 
             'super_agent_form':  AgentForm(
-                initial=Agent.objects.filter(wallet_provider=root,super=False),
-                root = root
+                initial=Agent.objects.filter(wallet_provider=root, super=False),
+                root=root
             )
         }
         return render(request, template_name=self.template_name, context=context)
 
-
     def post(self, request, *args, **kwargs):
+        """
+        Handles POST requests to the SuperAdminAgentsSetup view
+        """
         root = ExpiringToken.objects.get(key=self.kwargs['token']).user
-        # pop superagent data
-        data = request.POST.copy()
-        data.pop('msisdn',None)
+        data = request.POST.copy()          # pop superagent data
+        data.pop('msisdn', None)
         agentform = AgentFormSet(
             data,
-            queryset=Agent.objects.filter(
-                wallet_provider=root
-            ),
+            queryset=Agent.objects.filter(wallet_provider=root),
             prefix='agents',
             form_kwargs={'root': root}
         )
-        super_agent_form = AgentForm(
-            {'msisdn': request.POST.get('msisdn')}, root=root)
+        super_agent_form = AgentForm({'msisdn': request.POST.get('msisdn')}, root=root)
+
         if agentform.is_valid() and super_agent_form.is_valid():
             super_agent = super_agent_form.save(commit=False)
             super_agent.super = True
             super_agent.wallet_provider = root
-            
             agents_msisdn = [super_agent.msisdn]
             objs = agentform.save(commit=False)
             try:
@@ -254,26 +246,21 @@ class SuperAdminAgentsSetup(SuperRequiredMixin, SuperFinishedSetupMixin, View):
                 agents_msisdn.append(obj.msisdn)
 
             if self.env.str('CALL_WALLETS', 'TRUE') == 'TRUE':
-                transactions, error = self.validate_agent_wallet(
-                    request, agents_msisdn)
-                if not transactions:
-                        # handle non form errors
-                        context = {
-                            "non_form_error":error,
-                            "agentform": agentform,
-                            "super_agent_form": super_agent_form,
-                        }
-                        return render(request, template_name=self.template_name, context=context)
+                transactions, error = self.validate_agent_wallet(request, agents_msisdn)
+                if not transactions:                        # handle non form errors
+                    context = {
+                        "non_form_error": error,
+                        "agentform": agentform,
+                        "super_agent_form": super_agent_form,
+                    }
+                    return render(request, template_name=self.template_name, context=context)
 
-                # transactions exist # check if have error or not #
-                if self.error_exist(transactions):
+                if self.error_exist(transactions):          # transactions exist # check if have error or not #
                     return self.handle_form_errors(agentform, super_agent_form, transactions)
 
             super_agent.save()
             agentform.save()
-            
-            entity_setup = EntitySetup.objects.get(user=self.request.user,
-                                                   entity=root)
+            entity_setup = EntitySetup.objects.get(user=self.request.user, entity=root)
             entity_setup.agents_setup = True
             entity_setup.save()
             AGENT_CREATE_LOGGER.debug(f"""[AGENTS CREATED]
@@ -282,8 +269,8 @@ class SuperAdminAgentsSetup(SuperRequiredMixin, SuperFinishedSetupMixin, View):
             return HttpResponseRedirect(self.get_success_url(root))
         else:
             context = {
-                "agentform" : agentform,
-                "super_agent_form" : super_agent_form,
+                "agentform": agentform,
+                "super_agent_form": super_agent_form
             }
             return render(request, template_name=self.template_name, context=context)
 
@@ -307,10 +294,9 @@ class SuperAdminAgentsSetup(SuperRequiredMixin, SuperFinishedSetupMixin, View):
         Response-> {str(response.status_code)} -- {str(response.text)}""")
         if response.ok:
             response_dict = response.json()
-            transactions = response_dict.get('TRANSACTIONS',None)
+            transactions = response_dict.get('TRANSACTIONS', None)
             if not transactions:
-                error_message = response_dict.get(
-                    'MESSAGE', None) or _("Agents creation failed")
+                error_message = response_dict.get('MESSAGE', None) or _("Agents creation failed")
                 return None, error_message
             else:
                 for agent in transactions:
@@ -324,10 +310,12 @@ class SuperAdminAgentsSetup(SuperRequiredMixin, SuperFinishedSetupMixin, View):
         return None, _("Agents creation process stopped during an internal error,\
                 can you try again or contact you support team")
 
-    def handle_form_errors(self,agentform, super_agent_form, transactions):
-        # add errros from data to forms
+    def handle_form_errors(self, agentform, super_agent_form, transactions):
+        """
+        :return: Add errors from data to forms
+        """
         super_msisdn = super_agent_form.data.get('msisdn')
-        super_msg = self.get_msg(super_msisdn,transactions)
+        super_msg = self.get_msg(super_msisdn, transactions)
         if super_msg:
             super_agent_form.add_error('msisdn', super_msg)
         for form in agentform.forms:
@@ -342,16 +330,15 @@ class SuperAdminAgentsSetup(SuperRequiredMixin, SuperFinishedSetupMixin, View):
         }
         return render(self.request, template_name=self.template_name, context=context)
 
-    def error_exist(self,transactions):
-        for trx in  transactions:
-            valid = trx.get('USER_TYPE',None)
+    def error_exist(self, transactions):
+        for trx in transactions:
+            valid = trx.get('USER_TYPE', None)
             if not valid:
                 return True
-        return False           
+        return False
 
-    def get_msg(self,msisdn,transactions):
-        obj = list(filter(
-            lambda trx: self.msisdn_slice(trx['MSISDN']) == msisdn, transactions))
+    def get_msg(self, msisdn, transactions):
+        obj = list(filter(lambda trx: self.msisdn_slice(trx['MSISDN']) == msisdn, transactions))
         if not obj:
             return None
         obj = obj[0]
@@ -360,60 +347,54 @@ class SuperAdminAgentsSetup(SuperRequiredMixin, SuperFinishedSetupMixin, View):
             return None
         return obj.get('MESSAGE', "Unknown error")
 
-    def msisdn_slice(self,msisdn):
+    def msisdn_slice(self, msisdn):
         return msisdn[-11:]
+
 
 @method_decorator([setup_required], name='dispatch')
 class BalanceInquiry(RootRequiredMixin, View):
     """
-    View for super user to create Agents for the entity. 
+    View for super user to create Agents for the entity.
     """
     template_name = 'disbursement/balance_inquiry.html'
 
-
     def get(self, request, *args, **kwargs):
-
-        context = {
-            "form" : BalanceInquiryPinForm()
-        }
+        """
+        Handles GET requests to the balance inquiry view
+        """
+        context = {"form": BalanceInquiryPinForm()}
         return render(request, template_name=self.template_name, context=context)
 
     def post(self, request, *args, **kwargs):
-
+        """
+        Handles POST requests to the balance inquiry view
+        """
         form = BalanceInquiryPinForm(request.POST)
         if not form.is_valid():
-            context = {    
-                "form":form
-            }
+            context = {"form": form}
             return render(request, template_name=self.template_name, context=context)
-        
-        ok, error_or_balance = self.get_wallet_balance(
-            request, form.data.get('pin'))
+
+        ok, error_or_balance = self.get_wallet_balance(request, form.data.get('pin'))
         if ok:
-            context = {
-                "balance": error_or_balance,         
-            }
+            context = {"balance": error_or_balance}
         else:
-            context = {
-                "error_message": error_or_balance,
-            }
+            context = {"error_message": error_or_balance}
         form = BalanceInquiryPinForm()
-        context['form'] = form    
+        context['form'] = form
         return render(request, template_name=self.template_name, context=context)
-   
-    def get_wallet_balance(self, request,pin):
+
+    def get_wallet_balance(self, request, pin):
         import requests
         from payouts.utils import get_dot_env
         env = get_dot_env()
         superadmin = request.user.root.client.creator
         vmt = VMTData.objects.get(vmt=superadmin)
         data = vmt.return_vmt_data(VMTData.BALANCE_INQUIRY)
-        super_agent = Agent.objects.get(wallet_provider=request.user,super=True)
-        data["MSISDN"]=  super_agent.msisdn
-        data["PIN"]= pin
+        super_agent = Agent.objects.get(wallet_provider=request.user, super=True)
+        data["MSISDN"] = super_agent.msisdn
+        data["PIN"] = pin
         try:
-            response = requests.post(
-                env.str(vmt.vmt_environment), json=data, verify=False)
+            response = requests.post(env.str(vmt.vmt_environment), json=data, verify=False)
         except Exception as e:
             WALLET_API_LOGGER.debug(f"""[BALANCE INQUIRY ERROR]
             Users-> vmt(superadmin):{superadmin.username}
@@ -422,14 +403,13 @@ class BalanceInquiry(RootRequiredMixin, View):
                 can you try again or contact you support team")
 
         WALLET_API_LOGGER.debug(f"""[BALANCE INQUIRY]
-        Users-> vmt(superadmin):{superadmin.username}
+        Users-> vmt(superadmin): {superadmin.username}
         Response-> {str(response.status_code)} -- {str(response.text)}""")
         if response.ok:
             resp_json = response.json()
             if resp_json["TXNSTATUS"] == '200':
                 return True, resp_json['BALANCE']
-            error_message = resp_json.get(
-                'MESSAGE', None) or _("Balance inquiry failed")
+            error_message = resp_json.get('MESSAGE', None) or _("Balance inquiry failed")
             return False, error_message
         return False, _("Balance inquiry process stopped during an internal error,\
                 can you try again or contact you support team")

8830b524889fc02d6b359fc67b37ecd9ab95c7ab
Mohammad Mamdouh <mohamedmamdouh@paymobsolutions.com>
2020-03-05 12:43:03 +0200

    Improve: Reformat Doc model to be pep8 compliant

--- data/models/doc.py
+++ data/models/doc.py
@@ -6,30 +6,25 @@ from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from data.utils import pkgen, update_filename
 from users.models import CheckerUser
 
+from ..utils import pkgen, update_filename
+
+
 class Doc(models.Model):
+    """
+    Model for representing uploaded document object
+    """
     DISBURSEMENT = 1
     COLLECTION = 2
     types = (
         (DISBURSEMENT, 'DISBURSEMENT'),
         (COLLECTION, 'COLLECTION')
     )
-    id = models.CharField(primary_key=True, editable=False,
-                          unique=True, db_index=True, max_length=32, default=pkgen)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
-                              on_delete=models.CASCADE, related_name='doc')
-    file_category = models.ForeignKey(
-        'data.FileCategory', null=True, on_delete=models.SET_NULL, related_name='doc')
-    collection_data = models.ForeignKey(
-        'data.CollectionData', null=True, on_delete=models.CASCADE, related_name='collection_doc')
-    file = models.FileField(upload_to=update_filename,
-                            null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+
+    id = models.CharField(primary_key=True, editable=False, unique=True, db_index=True, max_length=32, default=pkgen)
+    file = models.FileField(upload_to=update_filename, null=True, blank=True)
     is_disbursed = models.BooleanField(default=False)
-    disbursed_by = models.ForeignKey(
-        "users.CheckerUser", on_delete=models.CASCADE, null=True, related_name='document')
     can_be_disbursed = models.BooleanField(default=False)
     is_processed = models.BooleanField(default=False)
     txn_id = models.CharField(max_length=16, null=True, blank=True)
@@ -37,22 +32,36 @@ class Doc(models.Model):
     total_amount = models.FloatField(default=False)
     total_count = models.PositiveIntegerField(default=False)
     type_of = models.PositiveSmallIntegerField(default=DISBURSEMENT, choices=types)
+    created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='doc')
+    disbursed_by = models.ForeignKey("users.CheckerUser", on_delete=models.CASCADE, related_name='document', null=True)
+    file_category = models.ForeignKey('data.FileCategory', on_delete=models.SET_NULL, related_name='doc', null=True)
     format = models.ForeignKey('data.Format', on_delete=models.DO_NOTHING, null=True)
+    collection_data = models.ForeignKey(
+            'data.CollectionData',
+            on_delete=models.CASCADE,
+            related_name='collection_doc',
+            null=True
+    )
 
     class Meta:
-        permissions = (("upload_file", "upload file"),
-                       ("delete_file", "delete file"),
-                       ("download_file_xlsx", "download xlsx file"),
-                       ("access_file", "access file"),
-                       ("download_file_txt", "download txt file"),
-                       ("edit_file_online", "Edit the file online"),
-                       ("process_file", "Can user process file data"),
-                       ("can_disburse", "Can disburse file data"),
-                       )
+        permissions = (
+            ("upload_file", "upload file"),
+            ("delete_file", "delete file"),
+            ("download_file_xlsx", "download xlsx file"),
+            ("access_file", "access file"),
+            ("download_file_txt", "download txt file"),
+            ("edit_file_online", "Edit the file online"),
+            ("process_file", "Can user process file data"),
+            ("can_disburse", "Can disburse file data"),
+        )
         verbose_name_plural = 'Documents'
         ordering = ('-created_at', )
 
     def delete(self, *args, **kwargs):
+        """
+        :return: Delete document file if it exists at the file system specifically at the MEDIA ROOT directory
+        """
         try:
             os.remove(os.path.join(settings.MEDIA_ROOT, self.file.name))
         except:
@@ -82,14 +91,15 @@ class Doc(models.Model):
 
     def can_user_disburse(self, checker):
         """"
-        Check if checker can disburse current documemt(self) or not.
+        Check if checker can disburse current document(self) or not.
         return tuple:
         can disburse:boolean
-        reason: reason why checker can not disburse 
+        reason: reason why checker can not disburse
         code: error code
         """
         reviews = self.reviews.all()
         reason = ''
+
         if checker.root.client.is_active:
             if self.can_be_disbursed and reviews.filter(is_ok=False).count() == 0:
                 if reviews.filter(is_ok=True).count() >= self.file_category.no_of_reviews_required:
@@ -110,23 +120,22 @@ class Doc(models.Model):
         return False, reason, code
 
     def disbursement_ratio(self):
-        """return disbursement success percentage"""
+        """
+        :return: disbursement success percentage
+        """
         if not self.is_processed or not self.can_be_disbursed:
             return 0
         disbursement_data_count = self.disbursement_data.count()
         if disbursement_data_count == 0:
             return 0
 
-        failed_disbursement_count = self.disbursement_data.filter(
-            is_disbursed=False).count()
-        success_percentage = (
-            (disbursement_data_count-failed_disbursement_count) * 100) / disbursement_data_count
-        success_percentage = round(
-            success_percentage, 2) if success_percentage != 0 else 0
+        failed_disbursement_count = self.disbursement_data.filter(is_disbursed=False).count()
+        success_percentage = ((disbursement_data_count-failed_disbursement_count) * 100) / disbursement_data_count
+        success_percentage = round(success_percentage, 2) if success_percentage != 0 else 0
+
         return success_percentage
 
-
-    def can_user_review(self,checker):
+    def can_user_review(self, checker):
         """
         return tuple
         (can_user_review, user_already_reviewed)
@@ -134,7 +143,7 @@ class Doc(models.Model):
         if self.reviews.filter(user_created=checker).exists():
             return False, True
         reviews = self.reviews.all()
-        
+
         can_review = False
         levels = CheckerUser.objects.filter(hierarchy=checker.hierarchy).values_list(
             'level__level_of_authority', flat=True)
@@ -144,14 +153,14 @@ class Doc(models.Model):
             can_review = True
         else:
             levels = [level for level in levels if level < checker_level]
-            levels = [max(levels,default=0), checker_level]
+            levels = [max(levels, default=0), checker_level]
             if reviews.filter(user_created__level__level_of_authority__in=levels):
                 can_review = True
-    
-        return (can_review , False)
-        
+
+        return can_review, False
+
     def is_reviews_completed(self):
         return self.reviews.filter(is_ok=True).count() >= self.file_category.no_of_reviews_required
 
     def is_reviews_rejected(self):
-        return self.reviews.filter(is_ok=False).count() != 0 
+        return self.reviews.filter(is_ok=False).count() != 0

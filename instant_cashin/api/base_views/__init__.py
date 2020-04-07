68ab48bc7dcc771ee81366aefa284e9349e2dccb
Mohammad Mamdouh <mohamedmamdouh@paymobsolutions.com>
2020-04-07 15:08:09 +0200

    Improve: Split instant APIViews to separate modules, handle budget inquiry

---
+++ instant_cashin/api/base_views/__init__.py
@@ -0,0 +1,1 @@
+from .budget_inquiry import BudgetInquiryAPIView
--- instant_cashin/api/urls.py
+++ instant_cashin/api/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 
-from .views import BudgetInquiryAPIView, InstantDisbursementAPIView
+from .base_views import BudgetInquiryAPIView
+from .views import InstantDisbursementAPIView
 
 
 app_name = 'instant_api'
-
 
 urlpatterns = [
     # User Inquiry Feature is DISABLED for now! and HIDDEN from the Docs.
     # path('inquire-user/', InstantUserInquiryAPIView.as_view(), name='inquire_user'),
 
-    path('budget-inquiry/', BudgetInquiryAPIView.as_view(), name='custom_budget_inquiry'),
+    path('budget-inquiry/', BudgetInquiryAPIView.as_view(), name='budget_inquiry'),
     path('disburse/', InstantDisbursementAPIView.as_view(), name='instant_disburse'),
 ]

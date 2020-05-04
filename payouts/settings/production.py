a2d158111959b4840917eaa9b3928fdd3a7cdfb6
Mohammad Mamdouh <mohamedmamdouh@paymobsolutions.com>
2020-05-04 13:01:54 +0200

    Improve: Refactor general server settings to its own script

--- payouts/settings/production.py
+++ payouts/settings/production.py
@@ -1,57 +1,16 @@
-from .base import *
+# -*- coding: utf-8 -*-
+from .server import *
 
-
-DEBUG = False
 
 ALLOWED_HOSTS += (
     'payouts.paymobsolutions.com',
     '54.91.163.127',
 )
 
-# Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env.str('DB_NAME'),
-        'USER': env.str('DB_USER'),
-        'PASSWORD': env.str('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '',
-        'CONN_MAX_AGE': 600,
-    }
-}
-
-# Email
-SERVER_EMAIL = 'confirmrequest@paymobsolutions.com'
-EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
-EMAIL_HOST = 'email-smtp.eu-west-1.amazonaws.com'
-EMAIL_PORT = 465
-EMAIL_HOST_USER = env.str('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
-
 # Email Reporting
-ADMINS = [
-    ('Mohamed Mamdouh', 'mohamedmamdouh@paymobsolutions.com'),
+ADMINS += [
     ('Omar Nawar', 'omarnawar@paymobsolutions.com')
 ]
 
-# SSL
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-# SECURE_SSL_REDIRECT = True        # Handled by nginx
-
-# Enable HTTP Strict Transport Security (HSTS)
-SECURE_HSTS_SECONDS = 31536000
-SECURE_HSTS_PRELOAD = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-# proxy_set_header X-Forwarded-Proto "https";  >>  Add to nginx
-
 # base url
 BASE_URL = 'https://payouts.paymobsolutions.com'
-
-# session expiration
-SESSION_EXPIRE_SECONDS = 300
-SESSION_EXPIRE_AT_BROWSER_CLOSE = True
-SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
---
+++ payouts/settings/server.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from .base import *
+
+
+DEBUG = False
+
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env.str('DB_NAME'),
+        'USER': env.str('DB_USER'),
+        'PASSWORD': env.str('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '',
+        'CONN_MAX_AGE': 600,
+    }
+}
+
+# Email
+SERVER_EMAIL = 'confirmrequest@paymobsolutions.com'
+EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
+EMAIL_HOST = 'email-smtp.eu-west-1.amazonaws.com'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = env.str('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
+
+# Email Reporting
+ADMINS = [
+    ('Mohamed Mamdouh', 'mohamedmamdouh@paymobsolutions.com')
+]
+
+# SSL
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_SSL_REDIRECT = True        # Handled by nginx
+
+# Sessions and Cookies
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = True
+
+# session expiration
+SESSION_EXPIRE_SECONDS = 300
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
+
+# Enable HTTP Strict Transport Security (HSTS)
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# proxy_set_header X-Forwarded-Proto "https";  >>  Add to nginx

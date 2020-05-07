679a0c27fbe75cfc6a0c813df996d934ece3c1bc
Mohammad Mamdouh <mohamedmamdouh@paymobsolutions.com>
2020-05-07 05:39:23 +0200

    Improve: Remove redundant code out of users mixin

--- users/mixins.py
+++ users/mixins.py
@@ -1,4 +1,7 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 class ParentPermissionMixin(object):
@@ -19,55 +22,56 @@ class ParentPermissionMixin(object):
 
 
 class RootRequiredMixin(LoginRequiredMixin):
+
     def dispatch(self, request, *args, **kwargs):
-        if not (request.user.is_authenticated and request.user.is_root):
+        if not request.user.is_root:
             return self.handle_no_permission()
         return super().dispatch(request, *args, **kwargs)
 
 
-class CollectionRootRequiredMixin(LoginRequiredMixin):
+class CollectionRootRequiredMixin(RootRequiredMixin):
+
     def dispatch(self, request, *args, **kwargs):
-        if not (request.user.is_authenticated and request.user.is_root and request.user.get_status(request) == 'collection'):
+        if not request.user.get_status(request) == 'collection':
             return self.handle_no_permission()
         return super().dispatch(request, *args, **kwargs)
 
 
-class DisbursementRootRequiredMixin(LoginRequiredMixin):
+class DisbursementRootRequiredMixin(RootRequiredMixin):
+
     def dispatch(self, request, *args, **kwargs):
-        if not (request.user.is_authenticated and request.user.is_root and request.user.get_status(request) == 'disbursement'):
+        if not request.user.get_status(request) == 'disbursement':
             return self.handle_no_permission()
         return super().dispatch(request, *args, **kwargs)
 
 
 class SuperRequiredMixin(LoginRequiredMixin):
+
     def dispatch(self, request, *args, **kwargs):
-        if not (request.user.is_authenticated and request.user.is_superadmin):
+        if not request.user.is_superadmin:
             return self.handle_no_permission()
         return super().dispatch(request, *args, **kwargs)
 
 
-class SuperOwnsClientRequiredMixin(LoginRequiredMixin):
+class SuperOwnsClientRequiredMixin(UserPassesTestMixin, LoginRequiredMixin):
     """
     Give the access permission of a certain view to only SuperAdmin users,
     Considering: SuperAdmin MUST owns that entity we're trying to manage.
     """
-    def dispatch(self, request, *args, **kwargs):
-        has_permission = False
 
-        if request.user.is_superadmin and request.user.is_authenticated:
-            entity_admin_username = request.resolver_match.kwargs.get('username')
+    def test_func(self):
+        """Test if the current SuperAdmin owns the entity we're trying to manage"""
+        if self.request.user.is_superadmin:
+            entity_admin_username = self.request.resolver_match.kwargs.get('username')
 
-            for client_obj in request.user.clients.all():
+            for client_obj in self.request.user.clients.all():
                 if client_obj.client.username == entity_admin_username:
-                    has_permission = True
+                    return True
 
-        if not has_permission:
-            return self.handle_no_permission()
-
-        return super().dispatch(request, *args, **kwargs)
+        return False
 
 
-class SuperOrRootOwnsCustomizedBudgetClientRequiredMixin(LoginRequiredMixin):
+class SuperOrRootOwnsCustomizedBudgetClientRequiredMixin(UserPassesTestMixin, LoginRequiredMixin):
     """
     Give the access permission of a certain view to only Super or Root users,
     Considering:
@@ -75,27 +79,20 @@ class SuperOrRootOwnsCustomizedBudgetClientRequiredMixin(LoginRequiredMixin):
             1) SuperAdmin MUST owns the entity being balance inquired at.
             2) And entity being balance inquired at MUST has custom budget.
     """
-    def dispatch(self, request, *args, **kwargs):
-        has_permission = False
 
-        if request.user.is_superadmin:
-            entity_admin_username = request.resolver_match.kwargs.get('username')
+    def test_func(self):
+        if self.request.user.is_superadmin:
+            entity_admin_username = self.request.resolver_match.kwargs.get('username')
 
-            for client_obj in request.user.clients.all():
+            for client_obj in self.request.user.clients.all():
                 if client_obj.client.username == entity_admin_username:
                     if client_obj.client.has_custom_budget:
-                        has_permission = True
+                        return True
 
-        if request.user.is_root:
-            has_permission = True
-
-        if not has_permission:
-            return self.handle_no_permission()
-
-        return super().dispatch(request, *args, **kwargs)
+        return self.request.user.is_root
 
 
-class SuperOwnsCustomizedBudgetClientRequiredMixin(LoginRequiredMixin):
+class SuperOwnsCustomizedBudgetClientRequiredMixin(UserPassesTestMixin, LoginRequiredMixin):
     """
     Give the access permission of a certain view to only SuperAdmin users,
     Considering:
@@ -103,21 +100,17 @@ class SuperOwnsCustomizedBudgetClientRequiredMixin(LoginRequiredMixin):
             1) SuperAdmin MUST owns the entity that we're trying to manage its custom budget.
             2) And that entity MUST has custom budget.
     """
-    def dispatch(self, request, *args, **kwargs):
-        has_permission = False
 
-        if request.user.is_superadmin:
-            entity_admin_username = request.resolver_match.kwargs.get('username')
+    def test_func(self):
+        if self.request.user.is_superadmin:
+            entity_admin_username = self.request.resolver_match.kwargs.get('username')
 
-            for client_obj in request.user.clients.all():
+            for client_obj in self.request.user.clients.all():
                 if client_obj.client.username == entity_admin_username:
                     if client_obj.client.has_custom_budget:
-                        has_permission = True
+                        return True
 
-        if not has_permission:
-            return self.handle_no_permission()
-
-        return super().dispatch(request, *args, **kwargs)
+        return False
 
 
 class SuperFinishedSetupMixin(LoginRequiredMixin):
@@ -125,6 +118,7 @@ class SuperFinishedSetupMixin(LoginRequiredMixin):
     Prevent superuser from accessing entity setup views if he already finished it.
     Must be used after SuperRequiredMixin.
     """
+
     def dispatch(self, request, *args, **kwargs):
         if not request.user.has_uncomplete_entity_creation():
             return self.handle_no_permission()
@@ -135,7 +129,8 @@ class InstantReviewerRequiredMixin(LoginRequiredMixin):
     """
     Prevent non logged-in or non instant-viewer users from accessing instant transactions.
     """
+
     def dispatch(self, request, *args, **kwargs):
-        if not (request.user.is_authenticated and request.user.is_instantapiviewer):
+        if not request.user.is_instantapiviewer:
             return self.handle_no_permission()
         return super().dispatch(request, *args, **kwargs)

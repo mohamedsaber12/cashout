ed1dcba2807506340c20f4a440503a6764a6fca0
Mohammad Mamdouh <mohamedmamdouh@paymobsolutions.com>
2020-08-19 12:14:57 +0200

    Test: Add factory model for creating VMT data object and agents

--- disbursement/factories.py
+++ disbursement/factories.py
@@ -1,30 +1,71 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 import factory
-import faker
-from django.contrib.auth.hashers import make_password
+from faker import Factory as fakeFactory
 
-from users.factories import DisbursementUserFactory
-from . import models
-
-fake = faker.Factory.create()
+from .models import Agent, VMTData
 
 
-class VMTDataFactory(factory.Factory):
+fake = fakeFactory.create()
+
+
+class VMTDataFactory(factory.django.DjangoModelFactory):
+    """
+    Factory model for creating corresponding VMT data object for the superadmin users
+    """
+
+    login_username = "DISBURSEMENT"
+    login_password = "DISBURSEMENT"
+    request_gateway_code = "DISBURSEMENT"
+    request_gateway_type = "DISBURSEMENT"
+    wallet_issuer = "VODAFONE"
+    vmt_environment = "STAGING"
+    vmt = factory.Sequence(lambda obj: "superadmin")
+
     class Meta:
-        model = models.VMTData
-
-    login_username = 'DISBURSEMENT'
-    login_password = 'DISBURSEMENT'
-    request_gateway_code = 'DISBURSEMENT'
-    request_gateway_type = 'DISBURSEMENT'
-    wallet_issuer = 'DISBURSEMENT'
-    vmt = factory.SubFactory(DisbursementUserFactory)
-    vmt_environment = 'STAGING'
+        model = VMTData
 
 
 class AgentFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = models.Agent
+    """
+    Factory model for creating agents
+    """
 
-    wallet_provider = factory.SubFactory(DisbursementUserFactory)
-    msisdn = fake.numerify(text='01#########')
-    pin = make_password('123456')
+    msisdn = factory.Sequence(lambda obj: fake.numerify(text="010########"))
+    pin = factory.Sequence(lambda obj: fake.boolean())
+    super = factory.Sequence(lambda obj: fake.boolean())
+    type = factory.Sequence(lambda obj: fake.random_element(["V", "E"]))
+    wallet_provider = factory.Sequence(lambda obj: "superadmin")
+
+    class Meta:
+        model = Agent
+
+
+class VariousAgentFactory:
+    """
+    Factory model consumes the main AgentFactory model to create super-agents and agents
+    for specific (wallet issuer / agent provider) user
+    """
+
+    @staticmethod
+    def super_agent(agent_provider):
+        """Create super-agent for specific user"""
+        return AgentFactory(msisdn="01006333714", pin=True, super=True, type="V", wallet_provider=agent_provider)
+
+    @staticmethod
+    def vodafone_agent(agent_provider):
+        """Create vodafone-agent for specific user"""
+        return AgentFactory(msisdn="01006332833", pin=True, super=False, type="V", wallet_provider=agent_provider)
+
+    @staticmethod
+    def etisalat_agent(agent_provider):
+        """Create etisalat-agent for specific user"""
+        return AgentFactory(msisdn="01100926774", pin=True, super=False, type="E", wallet_provider=agent_provider)
+
+    @staticmethod
+    def mandatory_agents(agent_provider):
+        """Create the mandatory agents to complete the onboarding setups of specific user (superadmin/admin)"""
+        VariousAgentFactory.super_agent(agent_provider=agent_provider)
+        VariousAgentFactory.vodafone_agent(agent_provider=agent_provider)
+        VariousAgentFactory.etisalat_agent(agent_provider=agent_provider)
--- users/factories.py
+++ users/factories.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import factory
+import random
 
+import factory
 from faker import Factory as fakeFactory
+
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import Permission
+
+from disbursement.factories import VariousAgentFactory, VMTDataFactory
 
 from .models import SuperAdminUser
 
@@ -18,9 +23,9 @@ class BaseUserFactory(factory.django.DjangoModelFactory):
     """
 
     username = factory.LazyFunction(fake.user_name)
-    password = factory.LazyFunction(fake.password)
+    password = factory.Sequence(lambda obj: make_password(fake.password))
     email = factory.LazyFunction(fake.email)
-    mobile_no = factory.LazyFunction(fake.numerify)
+    mobile_no = factory.Sequence(lambda obj: fake.numerify(text="010########"))
     first_name = factory.LazyFunction(fake.first_name)
     last_name = factory.LazyFunction(fake.last_name)
 
@@ -40,30 +45,40 @@ class VariousOnboardingSuperAdminUserFactory:
     Factory model for creating superadmin users based on the three business models of onboarding
     """
 
+    @classmethod
+    def create_new_superadmin(cls):
+        superadmin_user = SuperAdminUserFactory()
+        VMTDataFactory(vmt=superadmin_user)
+        return superadmin_user
+
     @staticmethod
     def default_vodafone():
         """Create superadmin user with the default vodafone onboarding setups permission"""
-        superadmin_user = SuperAdminUserFactory()
+        superadmin_user = VariousOnboardingSuperAdminUserFactory.create_new_superadmin()
         superadmin_user.user_permissions.add(
-                Permission.objects.get(content_type__app_label='users', codename='vodafone_default_onboarding')
+                Permission.objects.get(content_type__app_label="users", codename="vodafone_default_onboarding")
         )
         return superadmin_user
 
     @staticmethod
     def accept_vodafone():
         """Create superadmin user with the accept vodafone onboarding setups permission"""
-        superadmin_user = SuperAdminUserFactory()
-        superadmin_user.wallet_fees_profile = "Full"
+        superadmin_user = VariousOnboardingSuperAdminUserFactory.create_new_superadmin()
+        VariousAgentFactory.mandatory_agents(agent_provider=superadmin_user)
+        superadmin_user.wallet_fees_profile = random.choice(["Full", "Half", "No fees"])
         superadmin_user.user_permissions.add(
-                Permission.objects.get(content_type__app_label='users', codename='accept_vodafone_onboarding')
+                Permission.objects.get(content_type__app_label="users", codename="accept_vodafone_onboarding")
         )
+        superadmin_user.save()
         return superadmin_user
 
     @staticmethod
     def instant_apis():
         """Create superadmin user with the instant apis onboarding setups permission"""
-        superadmin_user = SuperAdminUserFactory()
+        superadmin_user = VariousOnboardingSuperAdminUserFactory.create_new_superadmin()
+        VariousAgentFactory.mandatory_agents(agent_provider=superadmin_user)
         superadmin_user.user_permissions.add(
-                Permission.objects.get(content_type__app_label='users', codename='instant_model_onboarding')
+                Permission.objects.get(content_type__app_label="users", codename="instant_model_onboarding")
         )
+        superadmin_user.save()
         return superadmin_user

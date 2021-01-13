ef221d2b1c3d06b85fabf5f7fa9ad47e264a9b2b
Mohamed Mamdouh <mahammad.mamdouh@gmail.com>
2021-01-13 17:00:00 +0200

    Improve: Update sidebar logo

--- users/models/brand.py
+++ users/models/brand.py
@@ -10,6 +10,6 @@ class Brand(models.Model):
     color = models.CharField(max_length=20, null=True, blank=True)
     logo = ProcessedImageField(upload_to='entities_logo',
                                format='JPEG',
-                               options={'quality': 60}, null=True, default='entities_logo/pm_name.png')
+                               options={'quality': 60}, null=True, default='entities_logo/send_new_logo.png')
 
     mail_subject = models.CharField(max_length=200, null=True, blank=True, default='Paymob Send')

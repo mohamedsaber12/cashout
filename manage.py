e95f7273335bdf1bc0c9370694789ae54da53f53
Mohamed Saber <mohamedsaber@paymob.com>
2021-02-14 16:20:27 +0200

    debuging tools

--- manage.py
+++ manage.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 import os
 import sys
+from django.conf import settings
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "payouts.settings")
     try:
         from django.core.management import execute_from_command_line
+        if settings.DEBUG:
+              if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
+                import ptvsd
+                ptvsd.enable_attach(address=('0.0.0.0', 3000))
+                print("Attached remote debugger")
+
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "

4a49c41d7f923343607a96582e8fe9e637cb5f17
Mohamed Awad <mohamedawad@paymob.com>
2021-03-03 12:07:53 +0200

    Improve:- add more information in logs of this command => update_users_with_new_root_column

--- users/management/commands/update_users_with_new_root_column.py
+++ users/management/commands/update_users_with_new_root_column.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from users.models import  RootUser, User
 
 class Command(BaseCommand):
@@ -13,24 +13,47 @@ class Command(BaseCommand):
 
         self.stdout.write(self.style.SUCCESS('start updating users with root ...'))
         self.stdout.write(self.style.SUCCESS('----------------------------------------'))
-
+        users_with_no_root = []
+        users_have_root_count = 0
         for current_user in current_users:
             # get root of current user
-            current_root = None
             try:
                 if current_user.is_root:
                     current_root = current_user
                 else:
                     current_root = RootUser.objects.get(hierarchy=current_user.hierarchy)
+                users_have_root_count += 1
             except User.DoesNotExist:
-                raise CommandError('Root of this user "%s" does not exist' % current_user)
-
-            # update each user with his root
-            current_user.root = current_root
-            current_user.save()
-            self.stdout.write(
-                self.style.SUCCESS(f"{current_user} updated successfully with root {current_root}")
-            )
+                users_with_no_root.append(current_user)
+                self.stdout.write(
+                    self.style.ERROR(
+                        'Error:-  Root of this user ==> "%s" does not exist' % current_user
+                    )
+                )
+            else:
+                # update each user with his root
+                current_user.root = current_root
+                current_user.save()
+                self.stdout.write(
+                    self.style.SUCCESS(
+                        f"{current_user} updated successfully with root {current_root}"
+                    )
+                )
 
         self.stdout.write(self.style.SUCCESS('----------------------------------------'))
         self.stdout.write(self.style.SUCCESS('finish updating users with root'))
+        self.stdout.write(self.style.SUCCESS('=========== Summary ==========='))
+
+        self.stdout.write(
+            self.style.SUCCESS(f"Number of Updated Users is : {users_have_root_count}")
+        )
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Number of Users that have no root is : {len(users_with_no_root)}")
+        )
+        # print users with no root
+        self.stdout.write(self.style.SUCCESS(f"Users With no root :- "))
+        for u in users_with_no_root:
+            self.stdout.write(self.style.SUCCESS(f"user ==> {u}, with id ==> {u.id}"))
+        if len(users_with_no_root) == 0:
+            self.stdout.write(self.style.SUCCESS(f"[]"))

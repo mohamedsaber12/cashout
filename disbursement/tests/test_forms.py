72f4d43678620791ba06687f21457247ddff8397
Mohamed Awad <mohamedawad@paymob.com>
2021-05-27 18:00:09 +0200

    Improve:- add new tests for models in disbursement application

--- disbursement/tests/test_forms.py
+++ disbursement/tests/test_forms.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from rest_framework import status
 from django.test import TestCase
 
 from disbursement.forms import (AgentForm, ExistingAgentForm, PinForm, BalanceInquiryPinForm,
@@ -14,6 +13,13 @@ MOBILE_INVALID_CHOICE_ERROR = 'Select a valid choice. 01021y79732 is not one of 
 PIN_IS_NUMERIC_ERROR = 'Pin must be numeric'
 PIN_IS_INVALID = 'Invalid pin'
 AMOUNT_ERROR = 'Enter a whole number.'
+INVALID_ISSUER_ERROR = 'issuer must be one of these \
+bank_card / Bank Card / vodafone / etisalat / orange / bank_wallet / aman'
+ACCOUNT_NUMBER_ERROR = 'Invalid Account number'
+CREDITOR_NAME_ERROR = 'Symbols like !%*+& not allowed in full name'
+FIRST_NAME_ERROR = 'Symbols like !%*+& not allowed in first name'
+LAST_NAME_ERROR = 'Symbols like !%*+& not allowed in last name'
+CREDITOR_NAME_INVALID = 'Invalid name'
 
 
 class AddAgentFormTests(TestCase):
@@ -183,3 +189,77 @@ class SingleStepTransactionFormTests(TestCase):
         form = SingleStepTransactionForm(data=form_data, checker_user=checker_user)
         self.assertEqual(form.is_valid(), False)
         self.assertEqual(form.errors['amount'], [AMOUNT_ERROR])
+
+    # test issuer exist in form data
+    def test_issuer_not_exist(self):
+        checker_user = CheckerUser(id=15, username='test_checker_user')
+        checker_user.root = self.root
+        form_data = {}
+        form = SingleStepTransactionForm(data=form_data, checker_user=checker_user)
+        self.assertEqual(form.is_valid(), False)
+        self.assertEqual(form.errors['issuer'], [REQUIRED_FIELD_ERROR])
+
+    # test issuer invalid in form data
+    def test_issuer_invalid(self):
+        checker_user = CheckerUser(id=15, username='test_checker_user')
+        checker_user.root = self.root
+        form_data = {'issuer': 'xyz'}
+        form = SingleStepTransactionForm(data=form_data, checker_user=checker_user)
+        self.assertEqual(form.is_valid(), False)
+        self.assertEqual(
+            form.errors['issuer'], ['Select a valid choice. xyz is not one of the available choices.'])
+
+    # test creditor_account_number in valid
+    def test_creditor_account_number_is_invalid(self):
+        checker_user = CheckerUser(id=15, username='test_checker_user')
+        checker_user.root = self.root
+        form_data = {'creditor_account_number': '12534', 'issuer': 'bank_card'}
+        form = SingleStepTransactionForm(data=form_data, checker_user=checker_user)
+        self.assertEqual(form.is_valid(), False)
+        self.assertEqual(form.errors['creditor_account_number'], [ACCOUNT_NUMBER_ERROR])
+
+    # test account name is invalid
+    def test_creditor_name_is_invalid(self):
+        checker_user = CheckerUser(id=15, username='test_checker_user')
+        checker_user.root = self.root
+        form_data = {'creditor_name': '', 'issuer': 'bank_card'}
+        form = SingleStepTransactionForm(data=form_data, checker_user=checker_user)
+        self.assertEqual(form.is_valid(), False)
+        self.assertEqual(form.errors['creditor_name'], [CREDITOR_NAME_INVALID])
+
+    # test account name has symbol
+    def test_creditor_name_has_symbol(self):
+        checker_user = CheckerUser(id=15, username='test_checker_user')
+        checker_user.root = self.root
+        form_data = {'creditor_name': 'ddf%!+uhy', 'issuer': 'bank_card'}
+        form = SingleStepTransactionForm(data=form_data, checker_user=checker_user)
+        self.assertEqual(form.is_valid(), False)
+        self.assertEqual(form.errors['creditor_name'], [CREDITOR_NAME_ERROR])
+
+    # test full name has symbol
+    def test_full_name_has_symbol(self):
+        checker_user = CheckerUser(id=15, username='test_checker_user')
+        checker_user.root = self.root
+        form_data = {'full_name': 'ddf%!+uhy', 'issuer': 'orange'}
+        form = SingleStepTransactionForm(data=form_data, checker_user=checker_user)
+        self.assertEqual(form.is_valid(), False)
+        self.assertEqual(form.errors['full_name'], [CREDITOR_NAME_ERROR])
+
+    # test first name has symbol
+    def test_first_name_has_symbol(self):
+        checker_user = CheckerUser(id=15, username='test_checker_user')
+        checker_user.root = self.root
+        form_data = {'first_name': 'ddf%!+uhy', 'issuer': 'aman'}
+        form = SingleStepTransactionForm(data=form_data, checker_user=checker_user)
+        self.assertEqual(form.is_valid(), False)
+        self.assertEqual(form.errors['first_name'], [FIRST_NAME_ERROR])
+
+    # test last name has symbol
+    def test_last_name_has_symbol(self):
+        checker_user = CheckerUser(id=15, username='test_checker_user')
+        checker_user.root = self.root
+        form_data = {'last_name': 'ddf%!+uhy', 'issuer': 'aman'}
+        form = SingleStepTransactionForm(data=form_data, checker_user=checker_user)
+        self.assertEqual(form.is_valid(), False)
+        self.assertEqual(form.errors['last_name'], [LAST_NAME_ERROR])
+

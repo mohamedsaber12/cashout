570a8a0d2a5f774d984d638bdfabed1b39ffd5a0
Mohamed Awad <mohamedawad@paymob.com>
2021-06-24 18:25:16 +0200

    Improve:- add new test cases to generic models in utilities application

--- utilities/tests/test_models.py
+++ utilities/tests/test_models.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from decimal import Decimal
 
 from django.test import TestCase
 
 from users.tests.factories import SuperAdminUserFactory, VMTDataFactory
+from users.models import RootUser
+
+from utilities.models import Budget, FeeSetup, CallWalletsModerator
 
 DISBURSEMENT = 1
 DISBURSEMENT_KEYS = ['LOGIN', 'PASSWORD', 'REQUEST_GATEWAY_CODE', 'REQUEST_GATEWAY_TYPE', 'WALLETISSUER', 'SENDERS',
@@ -220,4 +224,309 @@ class AbstractBaseVMTDataTests(TestCase):
         self.assertEqual(
             list(payload_without_pin.keys()),
             BALANCE_INQUIRY_PAYLOAD_KEYS
+        )
+
+
+class BudgetTests(TestCase):
+    def setUp(self):
+        self.root = RootUser(id=1, username='test_root_user')
+        self.root.root = self.root
+        self.root.save()
+        self.budget = Budget(disburser=self.root)
+        self.budget.save()
+
+    # test budget created successfully
+    def test_budget_created_successfully(self):
+        self.assertEqual(
+            self.budget.__str__(),
+            f"{self.budget.disburser.username} Custom Budget"
+        )
+
+    # test accumulate_amount_with_fees_and_vat with issuer vodafone
+    def test_accumulate_amount_with_fees_and_vat_vodafone(self):
+        fees_setup_vodafone = FeeSetup(budget_related=self.budget, issuer='vf',
+                                        fee_type='p', percentage_value=2.25)
+        fees_setup_vodafone.save()
+        self.assertEqual(
+            self.budget.accumulate_amount_with_fees_and_vat(200, 'V'),
+            Decimal('205.13')
+        )
+
+    # test accumulate_amount_with_fees_and_vat with issuer etisalat
+    def test_accumulate_amount_with_fees_and_vat_etisalat(self):
+        fees_setup_etisalat = FeeSetup(
+            budget_related=self.budget, issuer='es', fee_type='p',
+            percentage_value=2.25, min_value=10)
+        fees_setup_etisalat.save()
+        self.assertEqual(
+            self.budget.accumulate_amount_with_fees_and_vat(200, 'E'),
+            Decimal('211.4')
+        )
+
+    # test accumulate_amount_with_fees_and_vat with issuer orange
+    def test_accumulate_amount_with_fees_and_vat_orange(self):
+        fees_setup_orange = FeeSetup(
+            budget_related=self.budget, issuer='og', fee_type='p',
+            percentage_value=2.25, max_value=3)
+        fees_setup_orange.save()
+        self.assertEqual(
+            self.budget.accumulate_amount_with_fees_and_vat(200, 'O'),
+            Decimal('203.42')
+        )
+
+    # test accumulate_amount_with_fees_and_vat with issuer aman
+    def test_accumulate_amount_with_fees_and_vat_aman(self):
+        fees_setup_aman = FeeSetup(budget_related=self.budget, issuer='am',
+                                        fee_type='p', percentage_value=2.25)
+        fees_setup_aman.save()
+        self.assertEqual(
+                self.budget.accumulate_amount_with_fees_and_vat(200, 'A'),
+                Decimal('205.13')
+        )
+
+    # test accumulate_amount_with_fees_and_vat with issuer bank wallet
+    def test_accumulate_amount_with_fees_and_vat_bank_wallet(self):
+        fees_setup_bank_wallet = FeeSetup(budget_related=self.budget, issuer='bw',
+                                       fee_type='p', percentage_value=2.25)
+        fees_setup_bank_wallet.save()
+        self.assertEqual(
+                self.budget.accumulate_amount_with_fees_and_vat(200, 'B'),
+                Decimal('205.13')
+        )
+    # test accumulate_amount_with_fees_and_vat with fake fees
+    def test_accumulate_amount_with_fees_and_vat_fake_fees(self):
+        self.assertRaises(
+            ValueError,
+            self.budget.accumulate_amount_with_fees_and_vat,
+            200, 'C'
+        )
+
+    # test calculate_fees_and_vat_for_amount with issuer vodafone
+    def test_calculate_fees_and_vat_for_amount_vodafone(self):
+        fees_setup_vodafone = FeeSetup(budget_related=self.budget, issuer='vf',
+                                       fee_type='p', percentage_value=2.25)
+        fees_setup_vodafone.save()
+        fees, vat = self.budget.calculate_fees_and_vat_for_amount(200, 'V')
+        self.assertEqual(
+            fees,
+            Decimal('4.5')
+        )
+        self.assertEqual(
+            vat,
+            Decimal('0.63')
+        )
+
+    # test calculate_fees_and_vat_for_amount with issuer etisalat
+    def test_calculate_fees_and_vat_for_amount_etisalat(self):
+        fees_setup_etisalat = FeeSetup(
+                budget_related=self.budget, issuer='es', fee_type='p',
+                percentage_value=2.25, min_value=10)
+        fees_setup_etisalat.save()
+        fees, vat = self.budget.calculate_fees_and_vat_for_amount(200, 'E')
+        self.assertEqual(
+                fees,
+                Decimal('10')
+        )
+        self.assertEqual(
+                vat,
+                Decimal('1.4000')
+        )
+
+    # test calculate_fees_and_vat_for_amount with issuer orange
+    def test_calculate_fees_and_vat_for_amount_orange(self):
+        fees_setup_orange = FeeSetup(
+                budget_related=self.budget, issuer='og', fee_type='p',
+                percentage_value=2.25, max_value=3)
+        fees_setup_orange.save()
+        fees, vat = self.budget.calculate_fees_and_vat_for_amount(200, 'O')
+        self.assertEqual(
+                fees,
+                Decimal('3')
+        )
+        self.assertEqual(
+                vat,
+                Decimal('0.4200')
+        )
+
+    # test calculate_fees_and_vat_for_amount with issuer aman
+    def test_calculate_fees_and_vat_for_amount_aman(self):
+        fees_setup_aman = FeeSetup(budget_related=self.budget, issuer='am',
+                                   fee_type='p', percentage_value=2.25)
+        fees_setup_aman.save()
+        fees, vat = self.budget.calculate_fees_and_vat_for_amount(200, 'A')
+        self.assertEqual(
+                fees,
+                Decimal('4.5')
+        )
+        self.assertEqual(
+                vat,
+                Decimal('0.63')
+        )
+
+    # test calculate_fees_and_vat_for_amount with issuer bank wallet
+    def test_calculate_fees_and_vat_for_amount_bank_wallet(self):
+        fees_setup_bank_wallet = FeeSetup(budget_related=self.budget, issuer='bw',
+                                          fee_type='p', percentage_value=2.25)
+        fees_setup_bank_wallet.save()
+        fees, vat = self.budget.calculate_fees_and_vat_for_amount(200, 'B')
+        self.assertEqual(
+                fees,
+                Decimal('4.5000')
+        )
+        self.assertEqual(
+                vat,
+                Decimal('0.6300')
+        )
+
+    # test calculate_fees_and_vat_for_amount with issuer bank card
+    def test_calculate_fees_and_vat_for_amount_bank_card(self):
+        fees_setup_bank_wallet = FeeSetup(budget_related=self.budget, issuer='bc',
+                                          fee_type='f', fixed_value=20)
+        fees_setup_bank_wallet.save()
+        fees, vat = self.budget.calculate_fees_and_vat_for_amount(200, 'C')
+        self.assertEqual(
+            fees,
+            Decimal('20')
+        )
+        self.assertEqual(
+            vat,
+            Decimal('2.8000')
+        )
+
+    # test calculate_fees_and_vat_for_amount with issuer default
+    def test_calculate_fees_and_vat_for_amount_default(self):
+        fees_setup_bank_wallet = FeeSetup(budget_related=self.budget, issuer='bw',
+                                          fee_type='p', percentage_value=2.25)
+        fees_setup_bank_wallet.save()
+        fees, vat = self.budget.calculate_fees_and_vat_for_amount(200, 'default')
+        self.assertEqual(
+            fees,
+            Decimal('0')
+        )
+        self.assertEqual(
+            vat,
+            Decimal('0')
+        )
+
+    # test calculate_fees_and_vat_for_amount with fake fees
+    def test_calculate_fees_and_vat_for_amount_fake_fees(self):
+        self.assertRaises(
+            ValueError,
+            self.budget.calculate_fees_and_vat_for_amount,
+            200, 'C'
+        )
+
+    # test within_threshold
+    def test_within_threshold_with_None_value(self):
+        fees_setup_bank_wallet = FeeSetup(budget_related=self.budget, issuer='bw',
+                                          fee_type='p', percentage_value=2.25)
+        fees_setup_bank_wallet.save()
+        self.assertRaises(
+            ValueError,
+            self.budget.within_threshold,
+            200, None
+        )
+
+    # test within_threshold
+    def test_within_threshold(self):
+        fees_setup_bank_wallet = FeeSetup(budget_related=self.budget, issuer='bw',
+                                        fee_type='p', percentage_value=2.25)
+        fees_setup_bank_wallet.save()
+        self.budget.current_balance = Decimal('10000')
+        self.assertEqual(
+            self.budget.within_threshold(200, 'bank_wallet'),
+            True
+        )
+
+    #  test url for current budget is correct
+    def test_get_absolute_url(self):
+        self.assertEqual(
+            self.budget.get_absolute_url(),
+            f"/budget/edit/{self.budget.disburser.username}/"
+        )
+
+    # test update_disbursed_amount_and_current_balance
+    def test_update_disbursed_amount_and_current_balance_None_values(self):
+        self.assertRaises(
+            ValueError,
+            self.budget.update_disbursed_amount_and_current_balance,
+            200, None
+        )
+
+    # test update_disbursed_amount_and_current_balance
+    def test_update_disbursed_amount_and_current_balance(self):
+        fees_setup_bank_wallet = FeeSetup(budget_related=self.budget, issuer='bw',
+                                          fee_type='p', percentage_value=2.25)
+        fees_setup_bank_wallet.save()
+        self.assertEqual(
+            self.budget.update_disbursed_amount_and_current_balance(200, 'bank_wallet'),
+            True
+        )
+
+    # test return_disbursed_amount_for_cancelled_trx
+    def test_return_disbursed_amount_for_cancelled_trx_None_values(self):
+        self.assertRaises(
+            ValueError,
+            self.budget.return_disbursed_amount_for_cancelled_trx,
+            None
+        )
+
+    # test return_disbursed_amount_for_cancelled_trx
+    def test_return_disbursed_amount_for_cancelled_trx(self):
+        self.assertEqual(
+            self.budget.return_disbursed_amount_for_cancelled_trx(200),
+            True
+        )
+
+class FeeSetupTests(TestCase):
+
+    def setUp(self):
+        self.root = RootUser(id=1, username='test_root_user')
+        self.root.root = self.root
+        self.root.save()
+        self.budget = Budget(disburser=self.root)
+        self.budget.save()
+        self.fees_setup_bank_wallet = FeeSetup(
+            budget_related=self.budget, issuer='bw',
+            fee_type='p', percentage_value=2.25
+        )
+        self.fees_setup_bank_wallet.save()
+
+    # test feesSetup created successfully
+    def test_feesSetup_created_successfully(self):
+        self.assertEqual(
+            self.fees_setup_bank_wallet.__str__(),
+            f"{self.fees_setup_bank_wallet.fee_type_choice_verbose} setup for"
+            f" {self.fees_setup_bank_wallet.issuer_choice_verbose}"
+        )
+
+    # test issuer_choice_verbose
+    def test_issuer_choice_verbose(self):
+        self.assertEqual(
+            self.fees_setup_bank_wallet.issuer_choice_verbose,
+            'Bank Wallet'
+        )
+
+    # test fee_type_choice_verbose
+    def test_fee_type_choice_verbose(self):
+        self.assertEqual(
+            self.fees_setup_bank_wallet.fee_type_choice_verbose,
+            'Percentage Fee'
+        )
+
+
+class CallWalletsModeratorTests(TestCase):
+
+    def setUp(self):
+        self.root = RootUser(id=1, username='test_root_user')
+        self.root.root = self.root
+        self.root.save()
+        self.call_wallets_moderators = CallWalletsModerator(user_created=self.root)
+        self.call_wallets_moderators.save()
+
+    # test CallWalletsModerator object created successfully with root
+    def test_call_wallets_moderator_created_successfully(self):
+        self.assertEqual(
+            self.call_wallets_moderators.__str__(),
+            f"{self.root.username} moderator"
         )

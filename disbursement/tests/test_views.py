ae2bb429dc766d98ba8b57f5c97393f5c4b25889
Mohamed Awad <mohamedawad@paymob.com>
2021-07-12 18:32:03 +0200

    improve:- add new test cases for views in disbursement app

--- disbursement/tests/test_views.py
+++ disbursement/tests/test_views.py
@@ -9,8 +9,10 @@ from rest_framework_expiring_authtoken.models import ExpiringToken
 from users.tests.factories import (
     SuperAdminUserFactory, AdminUserFactory, VMTDataFactory
 )
-from users.models import Setup, Client as ClientModel, Brand, EntitySetup
-from utilities.models import Budget
+from users.models import (
+    Setup, Client as ClientModel, Brand, EntitySetup, CheckerUser, Levels
+)
+from utilities.models import Budget, CallWalletsModerator, FeeSetup
 from disbursement.models import Agent
 
 
@@ -136,30 +138,17 @@ class SuperAdminAgentsSetupTests(TestCase):
         self.entity_setup = EntitySetup(
             user=self.super_admin,
             entity=self.root,
-            agents_setup=True,
-            fees_setup=True
+            agents_setup=False,
+            fees_setup=False
         )
         self.entity_setup.save()
-        # self.agent = Agent(msisdn='01021469732', wallet_provider=self.root, super=True)
-        # self.agent.save()
-        # self.root.user_permissions. \
-        #     add(Permission.objects.get(
-        #         content_type__app_label='users', codename='has_disbursement')
-        # )
+
         self.client_user = ClientModel(client=self.root, creator=self.super_admin)
         self.client_user.save()
-        # self.setup = Setup(
-        #         user=self.root,
-        #         levels_setup=True,
-        #         maker_setup=True,
-        #         checker_setup=True,
-        #         category_setup=True,
-        #         pin_setup=True
-        # )
-        # self.setup.save()
-        # self.budget = Budget(disburser=self.root)
-        # self.budget.save()
-
+        CallWalletsModerator.objects.create(
+            user_created=self.root, disbursement=False, change_profile=False, set_pin=False,
+            user_inquiry=False, balance_inquiry=False
+        )
 
         self.request = RequestFactory()
         self.client = Client()
@@ -179,39 +168,152 @@ class SuperAdminAgentsSetupTests(TestCase):
             response = self.client.get(f'/client/creation/agents/{token.key}/')
             self.assertEqual(response.status_code, 200)
 
-    # def test_view_url_accessible_by_name(self):
-    #     self.client.force_login(self.root)
-    #     token, created = ExpiringToken.objects.get_or_create(user=self.root)
-    #     if created:
-    #         response = self.client.get(
-    #             reverse('disbursement:add_agents',
-    #                 kwargs={'token':token.key}
-    #             )
-    #         )
-    #         self.assertEqual(response.status_code, 200)
+    def test_view_url_accessible_by_name(self):
+        self.client.force_login(self.super_admin)
+        token, created = ExpiringToken.objects.get_or_create(user=self.root)
+        if created:
+            response = self.client.get(
+                reverse('disbursement:add_agents',
+                    kwargs={'token':token.key}
+                )
+            )
+            self.assertEqual(response.status_code, 200)
 
-    # def test_view_uses_correct_template(self):
-    #     self.client.force_login(self.root)
-    #     token, created = ExpiringToken.objects.get_or_create(user=self.root)
-    #     if created:
-    #         response = self.client.get(
-    #             reverse('disbursement:add_agents',
-    #                 kwargs={'token':token.key}
-    #             )
-    #         )
-    #         self.assertEqual(response.status_code, 200)
-    #         self.assertTemplateUsed(response, 'entity/add_agent.html')
+    def test_view_uses_correct_template(self):
+        self.client.force_login(self.super_admin)
+        token, created = ExpiringToken.objects.get_or_create(user=self.root)
+        if created:
+            response = self.client.get(
+                reverse('disbursement:add_agents',
+                    kwargs={'token':token.key}
+                )
+            )
+            self.assertEqual(response.status_code, 200)
+            self.assertTemplateUsed(response, 'entity/add_agent.html')
 
-    # def test_post_method(self):
-    #     self.client.force_login(self.root)
-    #
-    #     data = {
-    #         "pin": "123456",
-    #     }
-    #     response = self.client.post(
-    #             reverse('disbursement:balance_inquiry',
-    #                     kwargs={'username':self.root.username}
-    #                     ),
-    #             data
-    #     )
-    #     self.assertEqual(response.status_code, 200)
+    def test_post_method(self):
+        self.client.force_login(self.super_admin)
+        token, created = ExpiringToken.objects.get_or_create(user=self.root)
+        if created:
+            data = {
+                "agents-TOTAL_FORMS": 1,
+                "agents-INITIAL_FORMS": 0,
+                "agents-MIN_NUM_FORMS": 1,
+                "agents-MAX_NUM_FORMS": 1000,
+                "agents-0-id": "",
+                "agents-0-msisdn": "01006332833",
+                "agents-0-DELETE": "",
+                "msisdn": "01021469732"
+            }
+
+            response = self.client.post(
+                reverse('disbursement:add_agents',
+                    kwargs={'token':token.key}
+                ),
+                data
+            )
+            self.assertRedirects(
+                response,
+                f'/client/fees-setup/{token.key}/'
+            )
+
+
+class SingleStepTransactionsViewTests(TestCase):
+
+    def setUp(self):
+        self.super_admin = SuperAdminUserFactory()
+        self.vmt_data_obj = VMTDataFactory(vmt=self.super_admin)
+        self.root = AdminUserFactory(user_type=3)
+        self.root.root = self.root
+        self.brand = Brand(mail_subject='')
+        self.brand.save()
+        self.root.brand = self.brand
+        self.root.set_pin('123456')
+        self.root.save()
+        self.root.user_permissions. \
+            add(Permission.objects.get(
+                content_type__app_label='users', codename='has_disbursement')
+        )
+        self.root.user_permissions. \
+            add(Permission.objects.get(
+                content_type__app_label='users', codename='accept_vodafone_onboarding')
+        )
+        self.client_user = ClientModel(client=self.root, creator=self.super_admin)
+        self.client_user.save()
+        self.checker_user = CheckerUser(
+            id=15,
+            username='test_checker_user',
+            root=self.root,
+            user_type=2
+        )
+        self.checker_user.save()
+        self.level = Levels(
+            max_amount_can_be_disbursed=1200,
+            created=self.root
+        )
+        self.level.save()
+        self.checker_user.level = self.level
+        self.checker_user.save()
+        self.checker_user.user_permissions. \
+            add(Permission.objects.get(
+                content_type__app_label='users', codename='has_disbursement')
+        )
+        self.checker_user.user_permissions. \
+            add(Permission.objects.get(
+                content_type__app_label='users', codename='accept_vodafone_onboarding')
+        )
+        self.request = RequestFactory()
+        self.client = Client()
+        self.budget = Budget(disburser=self.root, current_balance=150)
+        self.budget.save()
+        fees_setup_vodafone = FeeSetup(budget_related=self.budget, issuer='vf',
+                                       fee_type='p', percentage_value=2.25)
+        fees_setup_vodafone.save()
+        fees_setup_bank_wallet = FeeSetup(budget_related=self.budget, issuer='bc',
+                                          fee_type='f', fixed_value=20)
+        fees_setup_bank_wallet.save()
+    def test_redirect_if_not_logged_in(self):
+        response = self.client.get(
+            reverse('disbursement:add_agents',
+                kwargs={'token':'token'}
+                )
+        )
+        self.assertRedirects(response, '/user/login/')
+
+    def test_view_url_exists_at_desired_location(self):
+        self.client.force_login(self.root)
+        response = self.client.get('/disburse/single-step/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        self.client.force_login(self.root)
+        response = self.client.get(
+            reverse('disbursement:single_step_list_create')
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        self.client.force_login(self.root)
+        response = self.client.get(
+            reverse('disbursement:single_step_list_create')
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'disbursement/single_step_trx_list.html')
+
+    def test_post_method(self):
+        self.client.force_login(self.checker_user)
+        data = {
+            "amount": 100,
+            "issuer": "vodafone",
+            "msisdn": '01021469732',
+            "pin": "123456",
+        }
+        response = self.client.post(
+            '%s?issuer=wallets' % (reverse('disbursement:single_step_list_create')),
+            data
+        )
+        redirect_url = str(response.url).split('?')[0]
+        self.assertEqual(
+            redirect_url,
+            '/disburse/single-step/'
+        )

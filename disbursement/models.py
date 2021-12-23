e47601ccedc48e1245f4adf0488e16e75ce09c96
Mohamed Awad <mohamedawad@paymob.com>
2021-12-23 13:29:06 +0200

    add null=true for reference id

--- disbursement/models.py
+++ disbursement/models.py
@@ -164,7 +164,8 @@ class DisbursementData(AbstractTimeStamp):
     reference_id = models.CharField(
         _('Reference ID'),
         max_length=30,
-        default='None'
+        default='None',
+        null=True
     )
     aman_obj = GenericRelation(
         "instant_cashin.AmanTransaction",

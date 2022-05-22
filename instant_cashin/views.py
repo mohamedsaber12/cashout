fe37534a0c5f47e2c2c23d88ee59f4fcf592c9bf
Mohamed Awad <mohamedawad@paymob.com>
2022-05-22 14:33:54 +0200

    Improve: add right sidebar filter to list wallets and list bank for integration admin

--- instant_cashin/views.py
+++ instant_cashin/views.py
@@ -1,22 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from datetime import datetime
 
 from django.contrib.auth import get_user_model
 from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
 from django.views.generic import ListView
+from django.utils.timezone import datetime, make_aware
 
 from disbursement.models import BankTransaction
 
 from .mixins import IntegrationUserAndSupportUserPassesTestMixin
 from .models import InstantTransaction
 from django.core.paginator import Paginator
-from data.tasks import (
-    ExportPortalRootOrDashboardUserTransactionsEwallets, ExportPortalRootOrDashboardUserTransactionsBanks
-)
-
 
 
 class InstantTransactionsListView(IntegrationUserAndSupportUserPassesTestMixin, ListView):
@@ -47,17 +43,35 @@ class InstantTransactionsListView(IntegrationUserAndSupportUserPassesTestMixin, 
             hierarchy_to_filter_with = user.hierarchy
         else:
             hierarchy_to_filter_with = self.request.user.hierarchy
-        queryset = super().get_queryset().filter(
-            from_user__hierarchy=hierarchy_to_filter_with
-        ).order_by("-created_at")
-                    
-        if self.request.GET.get('search'):                      # Handle search keywords if any
-            search_keys = self.request.GET.get('search')
-            queryset = queryset.filter(
-                Q(uid__iexact=search_keys)|
-                Q(anon_recipient__iexact=search_keys)|
-                Q(transaction_status_description__icontains=search_keys)
+        filter_dict = {
+            'from_user__hierarchy':hierarchy_to_filter_with,
+        }
+        # add filters to filter dict
+        if self.request.GET.get('number'):
+            filter_dict['anon_recipient__contains'] = self.request.GET.get('number')
+        if self.request.GET.get('issuer'):
+            filter_dict['issuer_type'] = self.request.GET.get('issuer')
+        if self.request.GET.get('start_date'):
+            start_date = self.request.GET.get('start_date')
+            first_day = datetime(
+                year=int(start_date.split('-')[0]),
+                month=int(start_date.split('-')[1]),
+                day=int(start_date.split('-')[2]),
             )
+            filter_dict['disbursed_date__gte'] = make_aware(first_day)
+        if self.request.GET.get('end_date'):
+            end_date = self.request.GET.get('end_date')
+            last_day = datetime(
+                year=int(end_date.split('-')[0]),
+                month=int(end_date.split('-')[1]),
+                day=int(end_date.split('-')[2]),
+                hour=23,
+                minute=59,
+                second=59,
+            )
+            filter_dict['disbursed_date__lte'] = make_aware(last_day)
+
+        queryset = super().get_queryset().filter(**filter_dict).order_by("-created_at")
         paginator = Paginator(queryset, 20)
         page = self.request.GET.get('page', 1)
         return paginator.get_page(page)
@@ -96,19 +110,37 @@ class BankTransactionsListView(IntegrationUserAndSupportUserPassesTestMixin, Lis
             hierarchy_to_filter_with = user.hierarchy
         else:
             hierarchy_to_filter_with = self.request.user.hierarchy
-        bank_trx_ids = super().get_queryset().filter(user_created__hierarchy=hierarchy_to_filter_with).\
-            filter(~Q(creditor_bank__in=["THWL", "MIDG"])).\
-            order_by("parent_transaction__transaction_id", "-id").distinct("parent_transaction__transaction_id").\
-            values_list("id", flat=True)
-        queryset = BankTransaction.objects.filter(id__in=bank_trx_ids).order_by("-created_at")
 
-        if self.request.GET.get('search'):                      # Handle search keywords if any
-            search_keys = self.request.GET.get('search')
-            queryset = queryset.filter(~Q(creditor_bank__in=["THWL", "MIDG"])).filter(
-                    Q(parent_transaction__transaction_id__iexact=search_keys)|
-                    Q(creditor_account_number__in=search_keys)|
-                    Q(creditor_bank__in=search_keys)
-            ).prefetch_related("children_transactions").distinct().order_by("-created_at")
+        filter_dict = {
+            'user_created__hierarchy': hierarchy_to_filter_with,
+        }
+        # add filters to filter dict
+        if self.request.GET.get('account_number'):
+            filter_dict['creditor_account_number__contains'] = self.request.GET.get('account_number')
+        if self.request.GET.get('start_date'):
+            start_date = self.request.GET.get('start_date')
+            first_day = datetime(
+                year=int(start_date.split('-')[0]),
+                month=int(start_date.split('-')[1]),
+                day=int(start_date.split('-')[2]),
+            )
+            filter_dict['disbursed_date__gte'] = make_aware(first_day)
+        if self.request.GET.get('end_date'):
+            end_date = self.request.GET.get('end_date')
+            last_day = datetime(
+                year=int(end_date.split('-')[0]),
+                month=int(end_date.split('-')[1]),
+                day=int(end_date.split('-')[2]),
+                hour=23,
+                minute=59,
+                second=59,
+            )
+            filter_dict['disbursed_date__lte'] = make_aware(last_day)
+
+        queryset = super().get_queryset().filter(**filter_dict). \
+            filter(~Q(creditor_bank__in=["THWL", "MIDG"])). \
+            order_by("parent_transaction__transaction_id", "-id", "-created_at"). \
+            distinct("parent_transaction__transaction_id")
         paginator = Paginator(queryset, 20)
         page = self.request.GET.get('page')
         return paginator.get_page(page)

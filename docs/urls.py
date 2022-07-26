37053957b1cdb55f170a972e34bb9acde44807eb
Mohamed Awad <mohamedawad@paymob.com>
2022-07-26 15:35:10 +0200

    Improve:- add initial view for swagger api requests

--- docs/urls.py
+++ docs/urls.py
@@ -1,5 +1,6 @@
 from django.urls import re_path, path
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
 
 from .views import serve_docs
 
@@ -8,9 +9,9 @@ app_name = 'docs'
 
 
 urlpatterns = [
-    path('docs/swagger-ui/', TemplateView.as_view(
+    path('docs/swagger-ui/', login_required(TemplateView.as_view(
             template_name='instant_cashin/swagger_api_docs.html',
             extra_context={'schema_url': 'openapi-schema'}
-        ), name='swagger-ui'),
+        )), name='swagger-ui'),
     re_path(r'^docs/(?P<path>.*)$', serve_docs, name='instant_docs'),
 ]

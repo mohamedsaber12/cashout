573fbff554a27a16d4bad457cc64fb2c17447809
Mohamed Saber <mohamedsaber@paymob.com>
2022-10-12 14:43:57 +0200

    handle user deactivation

--- users/sso.py
+++ users/sso.py
@@ -44,8 +44,8 @@ class SSOIntegration:
             url = f"{settings.IDMS_BASE_URL}v1/analytics/users/{user.idms_user_id}"
             resp = requests.put(url, json=payload)
             SSO_INTEGRATION_LOGGER.debug(
-                f"SSO INTEGRATION EDIT USER ON IDMS REQUEST [URL - CONTENT - STATUS_CODE - DELTA]"
-                f"[{resp.url}] - [{resp.content}] - [{resp.status_code}] "
+                f"SSO INTEGRATION EDIT USER ON IDMS REQUEST [URL - PAYLOAD - CONTENT - STATUS_CODE - DELTA]"
+                f"[{resp.url}] - [{payload}] - [{resp.content}] - [{resp.status_code}] "
                 f"- [{resp.elapsed.total_seconds()}]"
             )
         else:

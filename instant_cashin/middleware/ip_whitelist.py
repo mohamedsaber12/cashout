be837f869d9440aedb538253dc549b2e0ec5cdf8
Mohamed Saber <mohamedsaber@paymob.com>
2022-12-11 13:24:00 +0200

    add log to show request x forward

--- instant_cashin/middleware/ip_whitelist.py
+++ instant_cashin/middleware/ip_whitelist.py
@@ -24,15 +24,24 @@ class FilterIPMiddleware(object):
     def process_view(self, request, view_func, view_args, view_kwargs):
 
         whitelisted_ip = [
-            "34.251.217.198", "172.31.8.189", "172.31.23.159","75.2.116.217",
-            "99.83.194.66", "172.31.29.238", "52.31.227.79"
+            "34.251.217.198",
+            "172.31.8.189",
+            "172.31.23.159",
+            "75.2.116.217",
+            "99.83.194.66",
+            "172.31.29.238",
+            "52.31.227.79",
         ]
         if request.user.is_authenticated and request.user.is_instantapichecker:
-            ip = request.META.get('REMOTE_ADDR')
+            ip = request.META.get("REMOTE_ADDR")
             IP_LOGGER.debug(
-                f"[message] [IP] [{request.user.username}] -- request Ip :-  {ip}"
+                f"[message] [IP] [{request.user.username}] -- request Ip [REMOTE_ADDR] :-  {ip}"
             )
-            #if 'eksab' in request.user.root.username and ip not in whitelisted_ip:
-                #raise PermissionDenied()
+            x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+            IP_LOGGER.debug(
+                f"[message] [IP] [{request.user.username}] -- request Ip [REMOTE_ADDR] :-  {x_forwarded_for}"
+            )
+            # if 'eksab' in request.user.root.username and ip not in whitelisted_ip:
+            # raise PermissionDenied()
 
         return None

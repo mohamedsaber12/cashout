326837ba4e64185c59e62b2b811066e88ef9a54e
Mohamed Saber <mohamedsaber@paymob.com>
2023-01-11 15:05:17 +0200

    logout issue

--- users/views/main_views.py
+++ users/views/main_views.py
@@ -15,8 +15,7 @@ from rest_framework_expiring_authtoken.views import ObtainExpiringAuthToken
 
 from users.sso import SSOIntegration
 
-from ..forms import (CallbackURLEditForm, LevelEditForm, OTPTokenForm,
-                     ProfileEditForm)
+from ..forms import CallbackURLEditForm, LevelEditForm, OTPTokenForm, ProfileEditForm
 from ..mixins import ProfileOwnerOrMemberRequiredMixin
 from ..models import User
 
@@ -31,10 +30,10 @@ class ProfileView(ProfileOwnerOrMemberRequiredMixin, DetailView):
     """
 
     model = User
-    template_name = 'users/profile.html'
+    template_name = "users/profile.html"
 
     def get_object(self, queryset=None):
-        return get_object_or_404(User, username=self.kwargs['username'])
+        return get_object_or_404(User, username=self.kwargs["username"])
 
 
 class ProfileUpdateView(ProfileOwnerOrMemberRequiredMixin, UpdateView):
@@ -43,11 +42,11 @@ class ProfileUpdateView(ProfileOwnerOrMemberRequiredMixin, UpdateView):
     """
 
     model = User
-    template_name = 'users/profile_update.html'
+    template_name = "users/profile_update.html"
     form_class = ProfileEditForm
 
     def get_object(self, queryset=None):
-        return get_object_or_404(User, username=self.kwargs['username'])
+        return get_object_or_404(User, username=self.kwargs["username"])
 
 
 class LevelUpdateView(ProfileOwnerOrMemberRequiredMixin, UpdateView):
@@ -56,7 +55,7 @@ class LevelUpdateView(ProfileOwnerOrMemberRequiredMixin, UpdateView):
     """
 
     model = User
-    template_name = 'users/level_update.html'
+    template_name = "users/level_update.html"
     form_class = LevelEditForm
 
     def get_form_kwargs(self):
@@ -64,7 +63,7 @@ class LevelUpdateView(ProfileOwnerOrMemberRequiredMixin, UpdateView):
         pass request to form kwargs
         """
         kwargs = super().get_form_kwargs()
-        kwargs.update({'request': self.request})
+        kwargs.update({"request": self.request})
         return kwargs
 
     def get_success_url(self):
@@ -72,7 +71,7 @@ class LevelUpdateView(ProfileOwnerOrMemberRequiredMixin, UpdateView):
         return reverse("users:members")
 
     def get_object(self, queryset=None):
-        return get_object_or_404(User, username=self.kwargs['username'])
+        return get_object_or_404(User, username=self.kwargs["username"])
 
 
 class OTPLoginView(FormView):
@@ -80,8 +79,8 @@ class OTPLoginView(FormView):
     View for OTP login for checker users
     """
 
-    template_name = 'two_factor/login.html'
-    success_url = '/'
+    template_name = "two_factor/login.html"
+    success_url = "/"
 
     def post(self, request, *args, **kwargs):
         form = OTPTokenForm(data=request.POST, user=self.request.user)
@@ -99,18 +98,18 @@ class OTPLoginView(FormView):
 class RedirectPageView(View):
     """ """
 
-    template_name = 'users/redirect_page.html'
+    template_name = "users/redirect_page.html"
 
     def get(self, request, *args, **kwargs):
         if not (
             request.user.is_upmaker
             or (request.user.is_root and request.user.data_type() == 3)
         ):
-            return redirect('/')
-        status = request.GET.get('status', None)
-        if status is not None and status in ['disbursement', 'collection']:
-            request.session['status'] = status
-            return redirect(reverse(f'data:e_wallets_home'))
+            return redirect("/")
+        status = request.GET.get("status", None)
+        if status is not None and status in ["disbursement", "collection"]:
+            request.session["status"] = status
+            return redirect(reverse(f"data:e_wallets_home"))
 
         return render(request, self.template_name, {})
 
@@ -128,17 +127,17 @@ class ExpiringAuthToken(ObtainExpiringAuthToken):
 
         if serializer.is_valid():
             token, _ = self.model.objects.get_or_create(
-                user=serializer.validated_data['user']
+                user=serializer.validated_data["user"]
             )
 
             if token.expired():
                 # If the token is expired, generate a new one.
                 token.delete()
                 token = self.model.objects.create(
-                    user=serializer.validated_data['user']
+                    user=serializer.validated_data["user"]
                 )
 
-            data = {'token': token.key}
+            data = {"token": token.key}
             return Response(data)
 
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
@@ -152,21 +151,21 @@ def login_view(request):
     """
     context = {}
     login_template = (
-        'data/vodafone_login.html'
+        "data/vodafone_login.html"
         if "vodafone" in request.get_host()
-        else 'data/login.html'
+        else "data/login.html"
     )
     user = None
     print(request.GET)
     if request.GET.get("refresh"):
         return redirect("/")
-    
+
     if request.user.is_authenticated:
 
         # this is special case based on business demand for prevent \
         # two factor authentication for this admin => Careem_Admin
-        if request.user.username == 'Careem_Admin':
-            return redirect('data:main_view')
+        if request.user.username == "Careem_Admin":
+            return redirect("data:main_view")
 
         if (
             request.user.is_vodafone_default_onboarding
@@ -181,11 +180,11 @@ def login_view(request):
                 or request.user.is_root
                 or request.user.is_superadmin
             ):
-                return HttpResponseRedirect(reverse('two_factor:profile'))
-        return redirect('data:main_view')
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+                return HttpResponseRedirect(reverse("two_factor:profile"))
+        return redirect("data:main_view")
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
         # user = authenticate(request=request, username=username, password=password)
         sso = SSOIntegration()
         user = sso.authenticate(username, password)
@@ -198,8 +197,8 @@ def login_view(request):
 
                 # this is special case based on business demand for prevent \
                 # two factor authentication for this admin => Careem_Admin
-                if request.user.username == 'Careem_Admin':
-                    return HttpResponseRedirect(reverse('data:main_view'))
+                if request.user.username == "Careem_Admin":
+                    return HttpResponseRedirect(reverse("data:main_view"))
 
                 if (
                     request.user.is_vodafone_default_onboarding
@@ -214,12 +213,12 @@ def login_view(request):
                     ):
                         user.is_totp_verified = False
                         user.save()
-                        return HttpResponseRedirect(reverse('two_factor:profile'))
+                        return HttpResponseRedirect(reverse("two_factor:profile"))
 
                 if user.is_upmaker or (user.is_root and user.data_type() == 3):
-                    return HttpResponseRedirect(reverse('users:redirect'))
+                    return HttpResponseRedirect(reverse("users:redirect"))
 
-                return HttpResponseRedirect(reverse('data:main_view'))
+                return HttpResponseRedirect(reverse("data:main_view"))
             else:
                 FAILED_LOGIN_LOGGER.debug(
                     f"[message] [FAILED LOGIN] [{username}] -- "
@@ -231,14 +230,14 @@ def login_view(request):
                 f"[message] [API USER LOGIN ATTEMPT] [{username}] -- "
                 f"Failed Attempt from instant API user with username: {username}"
             )
-            context['error_invalid'] = "You're not permitted to login."
+            context["error_invalid"] = "You're not permitted to login."
             return render(request, login_template, context=context)
         else:
             # Bad login details were provided. So we can't log the user in.
             FAILED_LOGIN_LOGGER.debug(
                 f"[message] [FAILED LOGIN] [anonymous] -- Failed Login Attempt from user with username: {username}"
             )
-            context['error_invalid'] = 'Invalid login details supplied.'
+            context["error_invalid"] = "Invalid login details supplied."
             return render(request, login_template, context=context)
     else:
         idms_token = request.COOKIES.get("IDMS_TOKEN")
@@ -251,7 +250,7 @@ def login_view(request):
                     user_obj,
                     backend="django.contrib.auth.backends.ModelBackend",
                 )
-                return HttpResponseRedirect(reverse('data:main_view'))
+                return HttpResponseRedirect(reverse("data:main_view"))
         return render(request, login_template, context=context)
 
 
@@ -262,25 +261,25 @@ def ourlogout(request):
     """
 
     if isinstance(request.user, AnonymousUser):
-        return HttpResponseRedirect(reverse('users:user_login_view'))
+        return HttpResponseRedirect(reverse("users:user_login_view"))
 
     LOGOUT_LOGGER.debug(f"[message] [LOGOUT] [{request.user}] -- ")
     request.user.is_totp_verified = False
     request.user.save()
     logout(request)
-    response = HttpResponseRedirect(reverse('users:user_login_view'))
-    response.delete_cookie("IDMS_TOKEN", domain=".paymobsolutions.com")
+    response = HttpResponseRedirect(reverse("users:user_login_view"))
+    response.set_cookie("IDMS_TOKEN", "", domain=".paymobsolutions.com", max_age=1)
     return response
 
 
 class CallbackURLEdit(UpdateView):
 
     model = User
-    template_name = 'users/callback_url.html'
+    template_name = "users/callback_url.html"
     form_class = CallbackURLEditForm
 
     def get_object(self, queryset=None):
-        user = get_object_or_404(User, username=self.kwargs['username'])
+        user = get_object_or_404(User, username=self.kwargs["username"])
         return user.root.root
 
     def form_valid(self, form):
@@ -289,6 +288,6 @@ class CallbackURLEdit(UpdateView):
         return redirect(
             reverse(
                 "users:api_viewer_callback",
-                kwargs={'username': self.request.user.username},
+                kwargs={"username": self.request.user.username},
             )
         )
